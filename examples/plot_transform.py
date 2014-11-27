bfed336de02c383e5671d907e496361327c658d4
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2014-11-27 23:28:01 +0100

    Add transformations module

---
+++ examples/plot_transform.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+from pytransform.transformations import plot_transform
+
+
+plot_transform()
+plt.show()
---
+++ pytransform/transformations.py
@@ -0,0 +1,43 @@
+import numpy as np
+from .rotations import plot_basis
+
+
+def invert_transform(A2B):
+    """TODO document me"""
+    if A2B.shape != (4, 4):
+        raise ValueError("Transformation must have shape (4, 4) but has %s"
+                         % A2B.shape)
+    inv = np.empty((4, 4))
+    inv[-1, :] = np.array([0, 0, 0, 1])
+    inv[:3, :3] = A2B[:3, :3].T
+    inv[:3, -1] = -A2B[:3, -1]
+    return inv
+
+
+def translate_transform(A2B, pB):
+    """TODO document me"""
+    A2B = A2B.copy()
+    l = len(pB)
+    A2B[:l, -1] = pB
+    return A2B
+
+
+def vector_to_point(v):
+    """TODO document me"""
+    return np.hstack((v, 1))
+
+
+def transform(A2B, PB):
+    """TODO document me"""
+    PB = np.asarray(PB)
+    if PB.ndim == 1:
+        return np.dot(A2B, PB)
+    elif PB.ndim == 2:
+        return np.dot(PB, A2B.T)
+    else:
+        raise ValueError("Cannot transform array with more than 2 dimensions")
+
+
+def plot_transform(ax=None, A2B=np.eye(4), s=1.0, ax_s=1, **kwargs):
+    """TODO document me"""
+    return plot_basis(ax, A2B[:3, :3], A2B[:3, 3], s, ax_s, **kwargs)
