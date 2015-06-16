42e178a89e5e7a01864b2f6a1605599a85721860
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2015-06-16 23:45:05 +0200

    Increase test coverage

--- pytransform/test/test_transformations.py
+++ pytransform/test/test_transformations.py
@@ -37,3 +37,26 @@ def test_vector_to_point():
     p = random_vector(random_state)
     A2B = transform_from(R, p)
     pB = transform(A2B, pA)
+
+
+def test_transform():
+    PA = np.array([[1, 2, 3, 1],
+                   [2, 3, 4, 1]])
+
+    random_state = np.random.RandomState(0)
+    R = matrix_from(a=random_axis_angle(random_state))
+    p = random_vector(random_state)
+    A2B = transform_from(R, p)
+
+    PB = transform(A2B, PA)
+    p0B = transform(A2B, PA[0])
+    p1B = transform(A2B, PA[1])
+    assert_array_almost_equal(PB, np.array([p0B, p1B]))
+
+    caught_error = False
+    try:
+        transform(A2B, np.zeros((2, 2, 4)))
+    except ValueError:
+        caught_error = True
+    finally:
+        assert_true(caught_error)
