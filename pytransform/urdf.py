44f6af88aeb766d8faa5f20a6a954dc6eda886d4
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2016-06-19 23:55:29 +0200

    Document URDF module

--- pytransform/urdf.py
+++ pytransform/urdf.py
@@ -6,17 +6,47 @@ from .rotations import matrix_from_axis_angle
 
 
 class TransformManagerWithJoints(TransformManager):
+    """Transformation manager that includes joints that can be moved."""
     def __init__(self):
         super(TransformManagerWithJoints, self).__init__()
         self.default_transformations = {}
 
     def add_joint(self, joint_name, from_frame, to_frame, A2B, axis):
+        """Add joint.
+
+        Parameters
+        ----------
+        joint_name : string
+            Name of the joint
+
+        from_frame : string
+            Child link of the joint
+
+        to_frame : string
+            Parent link of the joint
+
+        A2B : array-like, shape (4, 4)
+            Transformation from child to parent
+
+        axis : array-like, shape (3,)
+            Rotation axis of the joint (defined in the child frame)
+        """
         self.add_transform(from_frame, to_frame, A2B)
         if joint_name is not None:
             self.default_transformations[joint_name] = (from_frame, to_frame,
                                                         A2B, axis)
 
     def set_joint(self, joint_name, angle):
+        """Set joint angle.
+
+        Parameters
+        ----------
+        joint_name : string
+            Name of the joint
+
+        angle : float
+            Joint angle in radians
+        """
         from_frame, to_frame, A2B, axis = self.default_transformations[joint_name]
         joint_rotation = matrix_from_axis_angle(np.hstack((axis, [angle])))
         joint2A = transform_from(joint_rotation, np.zeros(3))
@@ -24,6 +54,7 @@ class TransformManagerWithJoints(TransformManager):
 
 
 class Node(object):
+    """Node from URDF file."""
     def __init__(self, name, base):
         self.name = name
         self.base = base
@@ -32,8 +63,8 @@ class Node(object):
         self.joint_axis = None
 
 
-def matrix_from_rpy(rpy):
-    # fixed axis roll, pitch and yaw angles
+def _matrix_from_rpy(rpy):
+    """Rotation matrix from fixed axis roll, pitch and yaw angles."""
     roll, pitch, yaw = rpy
     ca1 = np.cos(yaw)
     sa1 = np.sin(yaw)
@@ -50,6 +81,22 @@ def matrix_from_rpy(rpy):
 
 
 def load_urdf(urdf_xml, tm=None):
+    """Load URDF file into transformation manager.
+
+    Parameters
+    ----------
+    urdf_xml : string
+        Robot definition in URDF
+
+    tm : TransformManager or TransformManagerWithJoints, optional
+        Transformation manager
+
+    Returns
+    -------
+    tm : TransformManager or TransformManagerWithJoints
+        Returns a new transformation manager if none has been given. Otherwise
+        the old transformation manager is returned.
+    """
     if tm is not None and not isinstance(tm, TransformManager):
         raise TypeError("Unkown transformation manager type '%s'"
                         % type(tm))
@@ -61,10 +108,10 @@ def load_urdf(urdf_xml, tm=None):
     else:
         robot_name = "world"
 
-    result = {}
+    nodes = {}
     for link in robot.findAll("link"):
         name = link["name"]
-        result[name] = Node(name, robot_name)
+        nodes[name] = Node(name, robot_name)
 
     for joint in robot.findAll("joint"):
         parent = joint.find("parent")
@@ -72,13 +119,13 @@ def load_urdf(urdf_xml, tm=None):
         child = joint.find("child")
         child_name = child["link"]
 
-        node = result[child_name]
+        node = nodes[child_name]
         node.base = parent_name
 
         offset = np.fromstring(joint.find("origin")["xyz"], sep=" ")
         roll_pitch_yaw = np.fromstring(joint.find("origin")["rpy"], sep=" ")
 
-        node.A2B = transform_from(matrix_from_rpy(roll_pitch_yaw), offset)
+        node.A2B = transform_from(_matrix_from_rpy(roll_pitch_yaw), offset)
 
         if joint["type"] == "revolute":
             node.joint_name = joint["name"]
@@ -87,7 +134,7 @@ def load_urdf(urdf_xml, tm=None):
     if tm is None:
         tm = TransformManagerWithJoints()
 
-    for node in result.values():
+    for node in nodes.values():
         if isinstance(tm, TransformManagerWithJoints):
             tm.add_joint(node.joint_name, node.name, node.base, node.A2B,
                          node.joint_axis)
