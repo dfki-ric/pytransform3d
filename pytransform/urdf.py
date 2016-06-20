21d9ec9acc6f71d678bc16db43ad0e2cf63f6c33
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2016-06-20 23:48:35 +0200

    Refactor

--- pytransform/urdf.py
+++ pytransform/urdf.py
@@ -118,7 +118,7 @@ def load_urdf(urdf_xml, tm=None):
 
     nodes = {}
     for link in robot.findAll("link"):
-        node = _parse_link(link, nodes, robot_name)
+        node = _parse_link(link, robot_name)
         nodes[node.name] = node
 
     for joint in robot.findAll("joint"):
@@ -134,51 +134,27 @@ def load_urdf(urdf_xml, tm=None):
         if parent is None:
             raise UrdfException("No parent specified in joint '%s'"
                                 % joint_name)
+        if not parent.has_attr("link"):
+            raise UrdfException("No parent link name given in joint '%s'."
+                                % joint_name)
+        parent_name = parent["link"]
+        if parent_name not in nodes:
+            raise UrdfException("Parent link '%s' of joint '%s' is not defined."
+                                % (parent_name, joint_name))
 
         child = joint.find("child")
         if child is None:
             raise UrdfException("No child specified in joint '%s'"
                                 % joint_name)
-
-        if not parent.has_attr("link"):
-            raise UrdfException("No parent link name given in joint '%s'."
-                                % joint_name)
-        parent_name = parent["link"]
-
         if not child.has_attr("link"):
             raise UrdfException("No child link name given in joint '%s'."
                                 % joint_name)
         child_name = child["link"]
-
         if child_name not in nodes:
             raise UrdfException("Child link '%s' of joint '%s' is not defined."
                                 % (child_name, joint_name))
-        node = nodes[child_name]
-        node.joint_name = joint_name
-        node.joint_type = joint["type"]
 
-        if parent_name not in nodes:
-            raise UrdfException("Parent link '%s' of joint '%s' is not defined."
-                                % (parent_name, joint_name))
-        node.base = parent_name
-
-        origin = joint.find("origin")
-        translation = np.zeros(3)
-        rotation = np.eye(3)
-        if origin is not None:
-            if origin.has_attr("xyz"):
-                translation = np.fromstring(origin["xyz"], sep=" ")
-            if origin.has_attr("rpy"):
-                roll_pitch_yaw = np.fromstring(origin["rpy"], sep=" ")
-                rotation = _matrix_from_rpy(roll_pitch_yaw)
-
-        node.A2B = transform_from(rotation, translation)
-
-        node.joint_axis = np.array([1, 0, 0])
-        if node.joint_type == "revolute":
-            axis = joint.find("axis")
-            if axis is not None and axis.has_attr("xyz"):
-                node.joint_axis = np.fromstring(axis["xyz"], sep=" ")
+        _parse_joint(joint, nodes[child_name], parent_name)
 
     if tm is None:
         tm = TransformManagerWithJoints()
@@ -194,7 +170,32 @@ def load_urdf(urdf_xml, tm=None):
     return tm
 
 
-def _parse_link(link, nodes, robot_name):
+def _parse_link(link, robot_name):
+    """Make node from link."""
     if not link.has_attr("name"):
         raise UrdfException("Link name is missing.")
     return Node(link["name"], robot_name)
+
+
+def _parse_joint(joint, node, parent_name):
+    """Update node with joint."""
+    node.joint_name = joint["name"]
+    node.joint_type = joint["type"]
+    node.base = parent_name
+
+    origin = joint.find("origin")
+    translation = np.zeros(3)
+    rotation = np.eye(3)
+    if origin is not None:
+        if origin.has_attr("xyz"):
+            translation = np.fromstring(origin["xyz"], sep=" ")
+        if origin.has_attr("rpy"):
+            roll_pitch_yaw = np.fromstring(origin["rpy"], sep=" ")
+            rotation = _matrix_from_rpy(roll_pitch_yaw)
+    node.A2B = transform_from(rotation, translation)
+
+    node.joint_axis = np.array([1, 0, 0])
+    if node.joint_type == "revolute":
+        axis = joint.find("axis")
+        if axis is not None and axis.has_attr("xyz"):
+            node.joint_axis = np.fromstring(axis["xyz"], sep=" ")
