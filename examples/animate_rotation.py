bfb95c1d8335acb11e7886f0861e060fc225cb15
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2018-09-16 00:19:52 +0200

    Make animation example

---
+++ examples/animate_rotation.py
@@ -0,0 +1,48 @@
+"""
+========
+Rotation
+========
+
+Animates a rotation about the x-axis.
+"""
+print(__doc__)
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.animation as animation
+from pytransform.plot_utils import Frame
+from pytransform.rotations import *
+
+
+def update_frame(step, n_frames, frame):
+    angle = 2.0 * np.pi * (step + 1) / n_frames
+    R = matrix_from_angle(0, angle)
+    A2B = np.eye(4)
+    A2B[:3, :3] = R
+    frame.set_data(A2B)
+    return frame
+
+
+if __name__ == "__main__":
+    n_frames = 50
+
+    fig = plt.figure(figsize=(5, 5))
+
+    ax = fig.add_subplot(111, projection="3d", aspect="equal")
+    ax.set_xlim((-1, 1))
+    ax.set_ylim((-1, 1))
+    ax.set_zlim((-1, 1))
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
+
+    frame = Frame(np.eye(4), label="rotating frame", s=0.5)
+    frame.add_frame(ax)
+
+    anim = animation.FuncAnimation(
+        fig, update_frame, n_frames, fargs=(n_frames, frame), interval=50,
+        blit=False)
+
+    plt.show()
