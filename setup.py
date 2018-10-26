5fc539d09d0ee577af7003b67355ea7f1b0037d9
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2018-10-27 00:04:42 +0200

    Use setuptools

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
-
-
-from distutils.core import setup
+from setuptools import setup
 import pytransform
 
 
 if __name__ == "__main__":
+    with open("README.md", "r") as f:
+        long_description = f.read()
     setup(name='pytransform',
           version=pytransform.__version__,
           author='Alexander Fabisch',
           author_email='afabisch@googlemail.com',
           url='https://git.hb.dfki.de/besman/pytransform',
           description='3D transformations for Python',
-          long_description=open('README.md').read(),
+          long_description=long_description,
+          long_description_content_type="text/markdown",
+          classifiers=[
+              "Programming Language :: Python :: 2",
+              "Programming Language :: Python :: 3",
+              "License :: OSI Approved :: BSD License",
+              "Operating System :: OS Independent",
+              "Topic :: Scientific/Engineering :: Mathematics",
+              "Topic :: Scientific/Engineering :: Visualization",
+          ],
           license='New BSD',
           packages=['pytransform'],
           requires=["numpy", "scipy", "lxml", "bs4"]
