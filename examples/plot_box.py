dcaa2903cdeca70fe40a364154d59bf0c12f6e7c
Alexander Fabisch <afabisch@googlemail.com>
2020-11-04 22:47:08 +0100

    Print all variables

--- examples/plot_box.py
+++ examples/plot_box.py
@@ -5,11 +5,10 @@ Plot Box
 """
 import numpy as np
 import matplotlib.pyplot as plt
-from pytransform3d.transformations import plot_transform
 from pytransform3d.plot_utils import plot_box
 
 
-from pytransform3d.rotations import unitz, perpendicular_to_vectors, norm_vector
+from pytransform3d.rotations import unitx, unitz, perpendicular_to_vectors, norm_vector
 from mpl_toolkits.mplot3d.art3d import Text3D
 def plot_length_variable(ax, start, end, name, color="k", **kwargs):  # TODO move to plot_utils
     direction = end - start
@@ -21,7 +20,12 @@ def plot_length_variable(ax, start, end, name, color="k", **kwargs):  # TODO mov
     ax.plot([start[0], mid1[0]], [start[1], mid1[1]], [start[2], mid1[2]], color=color)
     ax.plot([end[0], mid2[0]], [end[1], mid2[1]], [end[2], mid2[2]], color=color)
 
-    mark = norm_vector(perpendicular_to_vectors(direction, unitz)) * 0.03 * length
+    if np.linalg.norm(direction / length - unitz) < np.finfo(float).eps:
+        axis = unitx
+    else:
+        axis = unitz
+
+    mark = norm_vector(perpendicular_to_vectors(direction, axis)) * 0.03 * length
     mark_start1 = start + mark
     mark_start2 = start - mark
     mark_end1 = end + mark
@@ -43,10 +47,19 @@ def plot_length_variable(ax, start, end, name, color="k", **kwargs):  # TODO mov
 random_state = np.random.RandomState(42)
 ax = plot_box(size=[1, 1, 1], wireframe=False, alpha=0.1, color="k")
 plot_box(ax=ax, size=[1, 1, 1], wireframe=True, alpha=0.3)
-plot_transform(ax=ax, A2B=np.eye(4), s=0.3, lw=3)
 plot_length_variable(
     ax=ax,
     start=np.array([-0.5, -0.5, 0.55]), end=np.array([0.5, -0.5, 0.55]),
     name="a",
+    fontsize=14, fontfamily="serif")
+plot_length_variable(
+    ax=ax,
+    start=np.array([0.55, -0.5, 0.5]), end=np.array([0.55, 0.5, 0.5]),
+    name="b",
+    fontsize=14, fontfamily="serif")
+plot_length_variable(
+    ax=ax,
+    start=np.array([-0.55, -0.5, -0.5]), end=np.array([-0.55, -0.5, 0.5]),
+    name="c",
     fontsize=14, fontfamily="serif")
 plt.show()
