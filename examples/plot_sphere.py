8fe781ac0b14770c1f27068b4eb33bce5e13808e
Alexander Fabisch <afabisch@googlemail.com>
2020-11-04 21:58:59 +0100

    Plot sphere

--- examples/plot_sphere.py
+++ examples/plot_sphere.py
@@ -1,16 +1,16 @@
 """
-========
-Plot Box
-========
+===========
+Plot Circle
+===========
 """
 import numpy as np
 import matplotlib.pyplot as plt
 from pytransform3d.transformations import plot_transform
-from pytransform3d.plot_utils import plot_box
+from pytransform3d.plot_utils import plot_sphere
 
 
 random_state = np.random.RandomState(42)
-ax = plot_box(size=[1, 1, 1], wireframe=False, alpha=0.1, color="k")
-plot_box(ax=ax, size=[1, 1, 1], wireframe=True)
+ax = plot_sphere(radius=0.5, wireframe=False, alpha=0.1, color="k", n_steps=20)
+plot_sphere(ax=ax, radius=0.5, wireframe=True)
 plot_transform(ax=ax, A2B=np.eye(4), s=0.3, lw=3)
 plt.show()
