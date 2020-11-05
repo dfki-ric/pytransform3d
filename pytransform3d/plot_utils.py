de03fca71d163bb36323b35c66257bc8295818f3
Alexander Fabisch <afabisch@googlemail.com>
2020-11-05 21:45:16 +0100

    Move plot_length_variable

--- pytransform3d/plot_utils.py
+++ pytransform3d/plot_utils.py
@@ -8,6 +8,7 @@ try:
     from mpl_toolkits.mplot3d import proj3d
     from mpl_toolkits.mplot3d.art3d import Line3D, Text3D, Poly3DCollection
     from .transformations import transform
+    from .rotations import unitx, unitz, perpendicular_to_vectors, norm_vector
 
 
     class Frame(artist.Artist):
@@ -279,6 +280,67 @@ try:
             [start[2], start[2] + s * direction[2]],
             mutation_scale=20, arrowstyle=arrowstyle, **kwargs)
         ax.add_artist(axis_arrow)
+
+        return ax
+
+
+    def plot_length_variable(ax=None, start=np.zeros(3), end=np.ones(3), name="l", ax_s=1, color="k", **kwargs):
+        """Plot length and with text at its center.
+
+        Parameters
+        ----------
+        ax : Matplotlib 3d axis, optional (default: None)
+            If the axis is None, a new 3d axis will be created
+
+        start : array-like, shape (3,), optional (default: [0, 0, 0])
+            Start point
+
+        end : array-like, shape (3,), optional (default: [1, 1, 1])
+            End point
+
+        name : str, optional (default: 'l')
+            Text in the middle
+
+        ax_s : float, optional (default: 1)
+            Scaling of the new matplotlib 3d axis
+
+        color : str, optional (default: black)
+            Color in which the cylinder should be plotted
+
+        kwargs : dict, optional (default: {})
+            Additional arguments for the text, e.g. fontsize
+        """
+        if ax is None:
+            ax = make_3d_axis(ax_s)
+
+        direction = end - start
+        length = np.linalg.norm(direction)
+        mid1 = start + 0.4 * direction
+        mid2 = start + 0.6 * direction
+        mid = start + 0.45 * direction
+
+        ax.plot([start[0], mid1[0]], [start[1], mid1[1]], [start[2], mid1[2]], color=color)
+        ax.plot([end[0], mid2[0]], [end[1], mid2[1]], [end[2], mid2[2]], color=color)
+
+        if np.linalg.norm(direction / length - unitz) < np.finfo(float).eps:
+            axis = unitx
+        else:
+            axis = unitz
+
+        mark = norm_vector(perpendicular_to_vectors(direction, axis)) * 0.03 * length
+        mark_start1 = start + mark
+        mark_start2 = start - mark
+        mark_end1 = end + mark
+        mark_end2 = end - mark
+        ax.plot([mark_start1[0], mark_start2[0]],
+                [mark_start1[1], mark_start2[1]],
+                [mark_start1[2], mark_start2[2]],
+                color=color)
+        ax.plot([mark_end1[0], mark_end2[0]],
+                [mark_end1[1], mark_end2[1]],
+                [mark_end1[2], mark_end2[2]],
+                color=color)
+        ax.text(mid[0], mid[1], mid[2], name, zdir="x", **kwargs)
 
         return ax
 
