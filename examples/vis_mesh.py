bd0353da84ebe8eeabe5bcbf54db282eb47c2e54
Alexander Fabisch <afabisch@googlemail.com>
2020-11-23 21:50:14 +0100

    Add new mesh

--- examples/vis_mesh.py
+++ examples/vis_mesh.py
@@ -5,14 +5,16 @@ Visualize Mesh
 
 This example shows how to load an STL mesh. This example must be
 run from within the main folder because it uses a
-hard-coded path to the STL file.
+hard-coded path to the STL file. Press 'H' to print the viewer's
+help message to stdout.
 """
+print(__doc__)
 import numpy as np
 from pytransform3d import visualizer as pv
 
 
 fig = pv.figure()
-fig.plot_mesh(filename="test/test_data/cone.stl", s=5 * np.ones(3))
+fig.plot_mesh(filename="test/test_data/scan.stl", s=np.ones(3))
 fig.plot_transform(A2B=np.eye(4), s=0.3)
-fig.view_init()
+#fig.view_init()
 fig.show()
