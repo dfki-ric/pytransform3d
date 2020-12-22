0933647e7f1d4a9458fb1f91134aef27d3e18ac7
Alexander Fabisch <afabisch@googlemail.com>
2020-12-22 17:33:54 +0100

    Add box to gallery

--- examples/plot_box.py
+++ examples/plot_box.py
@@ -3,13 +3,17 @@
 Plot Box
 ========
 """
+print(__doc__)
+
+
 import numpy as np
 import matplotlib.pyplot as plt
-from pytransform3d.plot_utils import plot_box, plot_length_variable
+from pytransform3d.plot_utils import plot_box, plot_length_variable, remove_frame
 from pytransform3d.transformations import plot_transform
 
 
-ax = plot_box(size=[1, 1, 1], wireframe=False, alpha=0.1, color="k")
+plt.figure()
+ax = plot_box(size=[1, 1, 1], wireframe=False, alpha=0.1, color="k", ax_s=0.6)
 plot_transform(ax=ax)
 plot_box(ax=ax, size=[1, 1, 1], wireframe=True, alpha=0.3)
 plot_length_variable(
@@ -27,4 +31,5 @@ plot_length_variable(
     start=np.array([-0.55, -0.5, -0.5]), end=np.array([-0.55, -0.5, 0.5]),
     name="c",
     fontsize=14, fontfamily="serif")
+remove_frame(ax)
 plt.show()
