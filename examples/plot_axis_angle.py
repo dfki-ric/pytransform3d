8e257317c160988893b09a6a845fc52770910082
Alexander Fabisch <afabisch@googlemail.com>
2020-12-22 17:43:56 +0100

    Clean up axis-angle example

--- examples/plot_axis_angle.py
+++ examples/plot_axis_angle.py
@@ -14,11 +14,13 @@ from pytransform3d.rotations import (random_axis_angle, matrix_from_axis_angle,
                                      plot_basis, plot_axis_angle)
 
 
-original = random_axis_angle(np.random.RandomState(0))
+original = random_axis_angle(np.random.RandomState(5))
 ax = plot_axis_angle(a=original)
 for fraction in np.linspace(0, 1, 50):
     a = original.copy()
     a[-1] = fraction * original[-1]
     R = matrix_from_axis_angle(a)
     plot_basis(ax, R, alpha=0.2)
+plt.subplots_adjust(left=0, right=1, bottom=0, top=1.1)
+ax.view_init(azim=105, elev=12)
 plt.show()
