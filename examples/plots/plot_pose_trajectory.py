531744a6c37776ee03c5449ba1107d7adea8c065
Alexander Fabisch <alexander.fabisch@dfki.de>
2020-12-24 13:11:29 +0100

    Finetune examples for gallery

--- examples/plots/plot_pose_trajectory.py
+++ examples/plots/plot_pose_trajectory.py
@@ -16,12 +16,12 @@ from pytransform3d.trajectories import plot_trajectory
 
 n_steps = 100
 P = np.empty((n_steps, 7))
-P[:, 0] = np.linspace(-0.8, 0.8, n_steps) ** 2
-P[:, 1] = np.linspace(-0.8, 0.8, n_steps) ** 2
-P[:, 2] = np.linspace(-0.8, 0.8, n_steps)
+P[:, 0] = np.cos(np.linspace(-2 * np.pi, 2 * np.pi, n_steps))
+P[:, 1] = np.sin(np.linspace(-2 * np.pi, 2 * np.pi, n_steps))
+P[:, 2] = np.linspace(-1, 1, n_steps)
 q_end = np.array([0.0, 0.0, np.sqrt(0.5), np.sqrt(0.5)])
 P[:, 3:] = np.vstack([quaternion_slerp(q_id, q_end, t)
                       for t in np.linspace(0, 1, n_steps)])
 
-plot_trajectory(P=P, s=0.1, lw=1, c="k")
+ax = plot_trajectory(P=P, s=0.3, lw=2, c="k")
 plt.show()
--- examples/plots/plot_sphere.py
+++ examples/plots/plot_sphere.py
@@ -3,14 +3,19 @@
 Plot Sphere
 ===========
 """
+print(__doc__)
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 from pytransform3d.transformations import plot_transform
-from pytransform3d.plot_utils import plot_sphere
+from pytransform3d.plot_utils import plot_sphere, remove_frame
 
 
 random_state = np.random.RandomState(42)
-ax = plot_sphere(radius=0.5, wireframe=False, alpha=0.1, color="k", n_steps=20)
+ax = plot_sphere(
+    radius=0.5, wireframe=False, alpha=0.1, color="k", n_steps=20, ax_s=0.5)
 plot_sphere(ax=ax, radius=0.5, wireframe=True)
 plot_transform(ax=ax, A2B=np.eye(4), s=0.3, lw=3)
+remove_frame(ax)
 plt.show()
