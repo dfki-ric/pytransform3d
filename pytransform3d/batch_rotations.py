eddc650181d45fd688d66c12519e1bd8ee21a774
Alexander Fabisch <afabisch@googlemail.com>
2021-01-24 19:05:27 +0100

    Add docstring

--- pytransform3d/batch_rotations.py
+++ pytransform3d/batch_rotations.py
@@ -104,13 +104,42 @@ def active_matrices_from_angles(basis, angles, out=None):
     return out
 
 
-def active_matrices_from_intrinsic_euler_angles(basis1, basis2, basis3, e, out=None):
-    """TODO"""
+def active_matrices_from_intrinsic_euler_angles(
+        basis1, basis2, basis3, e, out=None):
+    """Compute active rotation matrices from intrinsic Euler angles.
+
+    Parameters
+    ----------
+    basis1 : int
+        Basis vector of first rotation. 0 corresponds to x axis, 1 to y axis,
+        and 2 to z axis.
+
+    basis2 : int
+        Basis vector of second rotation. 0 corresponds to x axis, 1 to y axis,
+        and 2 to z axis.
+
+    basis3 : int
+        Basis vector of third rotation. 0 corresponds to x axis, 1 to y axis,
+        and 2 to z axis.
+
+    e : array-like, shape (..., 3)
+        Euler angles
+
+    out : array, shape (..., 3, 3), optional (default: new array)
+        Output array to which we write the result
+
+    Returns
+    -------
+    Rs : array, shape (..., 3, 3)
+        Rotation matrices
+    """
     e = np.asarray(e)
     R_shape = e.shape + (3,)
     R_alpha = active_matrices_from_angles(basis1, e[..., 0].flat)
     R_beta = active_matrices_from_angles(basis2, e[..., 1].flat)
     R_gamma = active_matrices_from_angles(basis3, e[..., 2].flat)
+
+    # TODO refactor
     R = np.einsum("nij,njk->nik", np.einsum("nij,njk->nik", R_alpha, R_beta), R_gamma)
 
     if out is None:
