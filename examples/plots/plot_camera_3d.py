7d10ff619929fc986489c4c7d670766a2497e132
Alexander Fabisch <afabisch@googlemail.com>
2021-04-10 01:23:26 +0200

    Update plotting function, add simple example

---
+++ examples/plots/plot_camera_3d.py
@@ -0,0 +1,24 @@
+"""TODO"""
+print(__doc__)
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pytransform3d.camera as pc
+import pytransform3d.transformations as pt
+
+
+cam2world = np.eye(4)
+sensor_size = np.array([0.036, 0.024])
+intrinsic_matrix = np.array([
+    [0.05, 0, sensor_size[0] / 2.0],
+    [0, 0.05, sensor_size[1] / 2.0],
+    [0, 0, 1]
+])
+virtual_image_distance = 1
+
+ax = pt.plot_transform(A2B=cam2world, s=0.2)
+pc.plot_camera(
+    ax, cam2world=cam2world, M=intrinsic_matrix, sensor_size=sensor_size,
+    virtual_image_distance=virtual_image_distance)
+plt.show()
