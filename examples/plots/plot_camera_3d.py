8cf5621efdbf3d9ea85b36533bf1ea6ad60ae06b
Alexander Fabisch <afabisch@googlemail.com>
2021-04-11 23:01:11 +0200

    Document camera visualization

--- examples/plots/plot_camera_3d.py
+++ examples/plots/plot_camera_3d.py
@@ -1,4 +1,12 @@
-"""TODO"""
+"""
+===========================
+Camera Representation in 3D
+===========================
+
+This visualization is inspired by Blender's camera visualization. It will
+show the camera center, a virtual image plane at a desired distance to the
+camera center, and the top direction of the virtual image plane.
+"""
 print(__doc__)
 
 
@@ -8,7 +16,7 @@ import pytransform3d.camera as pc
 import pytransform3d.transformations as pt
 
 
-cam2world = np.eye(4)
+cam2world = pt.transform_from_pq([0, 0, 0, np.sqrt(0.5), -np.sqrt(0.5), 0, 0])
 # default parameters of a camera in Blender
 sensor_size = np.array([0.036, 0.024])
 intrinsic_matrix = np.array([
