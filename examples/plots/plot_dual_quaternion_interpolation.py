2ca3fa96f35b79ac995374abf8d52fe3c638444f
Alexander Fabisch <afabisch@googlemail.com>
2021-04-26 23:15:43 +0200

    Revert change to example

--- examples/plots/plot_dual_quaternion_interpolation.py
+++ examples/plots/plot_dual_quaternion_interpolation.py
@@ -23,7 +23,6 @@ import pytransform3d.plot_utils as ppu
 random_state = np.random.RandomState(21)
 pose1 = pt.random_transform(random_state)
 pose2 = pt.random_transform(random_state)
-pose2[:3, 3] += 0.01
 dq1 = pt.dual_quaternion_from_transform(pose1)
 dq2 = -pt.dual_quaternion_from_transform(pose2)
 Stheta1 = pt.exponential_coordinates_from_transform(pose1)
