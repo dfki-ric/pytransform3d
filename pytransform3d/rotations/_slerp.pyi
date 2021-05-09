74dffec867214ffb566b71bccb6a77c8aefb9ff8
Alexander Fabisch <afabisch@googlemail.com>
2021-05-09 18:37:53 +0200

    Type annotations for full rotations module

---
+++ pytransform3d/rotations/_slerp.pyi
@@ -0,0 +1,11 @@
+import numpy as np
+import numpy.typing as npt
+
+
+def axis_angle_slerp(start: npt.ArrayLike, end: npt.ArrayLike, t: float) -> np.ndarray: ...
+
+
+def quaternion_slerp(start: npt.ArrayLike, end: npt.ArrayLike, t: float) -> np.ndarray: ...
+
+
+def slerp_weights(angle: float, t: float) -> np.ndarray: ...
