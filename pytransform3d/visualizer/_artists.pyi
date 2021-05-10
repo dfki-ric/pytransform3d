4adca46b6bdbec11f7fa6961ba125ffe4897e26d
Alexander Fabisch <afabisch@googlemail.com>
2021-05-10 22:41:14 +0200

    Type hints for visualizer

---
+++ pytransform3d/visualizer/_artists.pyi
@@ -0,0 +1,118 @@
+import typing
+
+import numpy as np
+import numpy.typing as npt
+import open3d as o3d
+from typing import List, Union, Any, Dict
+from ..transform_manager import TransformManager
+if typing.TYPE_CHECKING:
+    from ._figure import Figure
+
+
+class Artist:
+    def add_artist(self, figure: Figure): ...
+
+    @property
+    def geometries(self) -> List[o3d.geometry.Geometry3D]: ...
+
+
+class Line3D(Artist):
+    def __init__(self, P: npt.ArrayLike, c: npt.ArrayLike = ...): ...
+
+    def set_data(self, P: npt.ArrayLike,
+                 c: Union[None, npt.ArrayLike] = ...): ...
+
+    @property
+    def geometries(self) -> List[o3d.geometry.Geometry3D]: ...
+
+
+class Frame(Artist):
+    def __init__(self, A2B: npt.ArrayLike,
+                 label: Union[None, str] = ..., s: float = ...): ...
+
+    def set_data(self, A2B: npt.ArrayLike, label: Union[None, str] = ...): ...
+
+    @property
+    def geometries(self) -> List[o3d.geometry.Geometry3D]: ...
+
+
+class Trajectory(Artist):
+    def __init__(self, H: npt.ArrayLike, n_frames: int = ..., s: float = ...,
+                 c: npt.ArrayLike = ...): ...
+
+    def set_data(self, H: npt.ArrayLike): ...
+
+    @property
+    def geometries(self) -> List[o3d.geometry.Geometry3D]: ...
+
+
+class Sphere(Artist):
+    def __init__(self, radius: float = ..., A2B: npt.ArrayLike = ...,
+                 resolution: int = ..., c: Union[None, npt.ArrayLike] = ...): ...
+
+    def set_data(self, A2B: npt.ArrayLike): ...
+
+    @property
+    def geometries(self) -> List[o3d.geometry.Geometry3D]: ...
+
+
+class Box(Artist):
+    def __init__(self, size: npt.ArrayLike = ..., A2B: npt.ArrayLike = ...,
+                 c: Union[None, npt.ArrayLike] = ...): ...
+
+    def set_data(self, A2B: npt.ArrayLike): ...
+
+    @property
+    def geometries(self) -> List[o3d.geometry.Geometry3D]: ...
+
+
+class Cylinder(Artist):
+    def __init__(self, length: float = ..., radius: float = ...,
+                 A2B: npt.ArrayLike = ..., resolution: int = ...,
+                 split: int = ..., c: Union[None, npt.ArrayLike] = ...): ...
+
+    def set_data(self, A2B: npt.ArrayLike): ...
+
+    @property
+    def geometries(self) -> List[o3d.geometry.Geometry3D]: ...
+
+
+class Mesh(Artist):
+    def __init__(self, filename: str, A2B: npt.ArrayLike = ...,
+                 s: npt.ArrayLike = ..., c: Union[None, npt.ArrayLike] = ...): ...
+
+    def set_data(self, A2B: npt.ArrayLike): ...
+
+    @property
+    def geometries(self) -> List[o3d.geometry.Geometry3D]: ...
+
+
+class Camera(Artist):
+    def __init__(
+            self, M: npt.ArrayLike, cam2world: Union[None, npt.ArrayLike] = ...,
+            virtual_image_distance: float = ...,
+            sensor_size: npt.ArrayLike = ..., strict_check: bool = ...): ...
+
+    def set_data(
+            self, M: Union[None, npt.ArrayLike],
+            cam2world: Union[None, npt.ArrayLike],
+            virtual_image_distance: Union[None, float],
+            sensor_size: Union[None, npt.ArrayLike]): ...
+
+    @property
+    def geometries(self) -> List[o3d.geometry.Geometry3D]: ...
+
+
+class Graph(Artist):
+    def __init__(
+            self, tm: TransformManager, frame: str, show_frames: bool = ...,
+            show_connections: bool = ..., show_visuals: bool = ...,
+            show_collision_objects: bool = ..., show_name: bool = ...,
+            whitelist: Union[None, List[str]] = ..., s: float = ...): ...
+
+    def _objects_to_artists(self, objects: List[Any]) -> Dict[str, Artist]: ...
+
+    def set_data(self): ...
+
+    @property
+    def geometries(self) -> List[o3d.geometry.Geometry3D]: ...
