7ecd7235933eb41d0082e8d7cd2c4e2cae53a3b0
Alexander Fabisch <afabisch@googlemail.com>
2021-05-10 22:57:59 +0200

    Type hints for batch_rotations

---
+++ pytransform3d/batch_rotations.pyi
@@ -0,0 +1,64 @@
+import numpy as np
+import numpy.typing as npt
+from typing import Union
+
+
+def norm_vectors(
+        V: npt.ArrayLike,
+        out: Union[npt.ArrayLike, None] = ...) -> np.ndarray: ...
+
+
+def angles_between_vectors(A: npt.ArrayLike, B: npt.ArrayLike) -> np.ndarray: ...
+
+
+def active_matrices_from_angles(
+        basis: int, angles: npt.ArrayLike,
+        out: Union[npt.ArrayLike, None] = ...) -> np.ndarray: ...
+
+
+def active_matrices_from_intrinsic_euler_angles(
+        basis1: int, basis2: int, basis3: int, e: npt.ArrayLike,
+        out: Union[npt.ArrayLike, None] = ...) -> np.ndarray: ...
+
+
+def active_matrices_from_extrinsic_euler_angles(
+        basis1: int, basis2: int, basis3: int, e: npt.ArrayLike,
+        out: Union[npt.ArrayLike, None] = ...) -> np.ndarray: ...
+
+
+def matrices_from_compact_axis_angles(
+        A: Union[npt.ArrayLike, None] = ...,
+        axes: Union[np.ndarray, None] = ...,
+        angles: Union[np.ndarray, None] = ...,
+        out: Union[np.ndarray, None] = ...) -> np.ndarray: ...
+
+
+def axis_angles_from_matrices(
+        Rs: npt.ArrayLike, traces: Union[np.ndarray, None] = ...,
+        out: Union[np.ndarray, None] = ...) -> np.ndarray: ...
+
+
+def cross_product_matrices(V: npt.ArrayLike) -> np.ndarray: ...
+
+
+def matrices_from_quaternions(
+        Q: npt.ArrayLike, normalize_quaternions: bool = ...,
+        out: Union[np.ndarray, None] = ...) -> np.ndarray: ...
+
+
+def quaternions_from_matrices(
+        Rs: npt.ArrayLike,
+        out: Union[np.ndarray, None] = ...) -> np.ndarray: ...
+
+
+def quaternion_slerp_batch(
+        start: npt.ArrayLike, end: npt.ArrayLike,
+        t: npt.ArrayLike) -> np.ndarray: ...
+
+
+def batch_concatenate_quaternions(
+        Q1: npt.ArrayLike, Q2: npt.ArrayLike,
+        out: Union[np.ndarray, None] = ...) -> np.ndarray: ...
+
+
+def batch_q_conj(Q: npt.ArrayLike) -> np.ndarray: ...
