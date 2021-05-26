9ab7c72d382903b371e1797a28a34a068a726f11
Alexander Fabisch <afabisch@googlemail.com>
2021-05-27 00:12:30 +0200

    Test plot functions

--- pytransform3d/test/test_plot_utils.py
+++ pytransform3d/test/test_plot_utils.py
@@ -5,8 +5,10 @@ except ImportError:
     from nose import SkipTest
     raise SkipTest("matplotlib is required for these tests")
 from pytransform3d.plot_utils import (
-    make_3d_axis, remove_frame, Frame, LabeledFrame, Trajectory)
-from nose.tools import assert_equal, assert_less
+    make_3d_axis, remove_frame, Frame, LabeledFrame, Trajectory,
+    plot_box, plot_sphere, plot_cylinder, plot_mesh, plot_vector,
+    plot_length_variable)
+from nose.tools import assert_equal, assert_less, assert_greater_equal
 
 
 def test_make_3d_axis():
@@ -55,34 +57,125 @@ def test_frame_removed():
 
 
 def test_frame():
-    ax = make_3d_axis(1)
+    ax = make_3d_axis(1.0)
     try:
         frame = Frame(np.eye(4), label="Frame", s=0.1)
         frame.add_frame(ax)
         assert_equal(len(ax.lines), 4)  # 3 axes and black line to text
-        assert_equal(len(ax.texts), 1)
+        assert_equal(len(ax.texts), 1)  # label
     finally:
         ax.remove()
 
 
 def test_labeled_frame():
-    ax = make_3d_axis(1)
+    ax = make_3d_axis(1.0)
     try:
         frame = LabeledFrame(np.eye(4), label="Frame", s=0.1)
         frame.add_frame(ax)
         assert_equal(len(ax.lines), 4)  # 3 axes and black line to text
-        assert_equal(len(ax.texts), 4)
+        assert_equal(len(ax.texts), 4)  # label and 3 axis labels
     finally:
         ax.remove()
 
 
 def test_trajectory():
-    ax = make_3d_axis(1)
+    ax = make_3d_axis(1.0)
     try:
         trajectory = Trajectory(
             np.array([np.eye(4), np.eye(4)]), s=0.1, n_frames=2)
         trajectory.add_trajectory(ax)
         assert_equal(len(ax.lines), 7)  # 2 * 3 axes + connection line
         assert_equal(len(ax.artists), 1)  # arrow
+    finally:
+        ax.remove()
+
+
+def test_plot_box():
+    ax = make_3d_axis(1.0)
+    try:
+        plot_box(ax, wireframe=False)
+        assert_equal(len(ax.collections), 1)
+    finally:
+        ax.remove()
+
+
+def test_plot_box_wireframe():
+    ax = make_3d_axis(1.0)
+    try:
+        plot_box(ax, wireframe=True)
+        assert_greater_equal(len(ax.lines), 1)
+    finally:
+        ax.remove()
+
+
+def test_plot_sphere():
+    ax = make_3d_axis(1.0)
+    try:
+        plot_sphere(ax, wireframe=False)
+        assert_equal(len(ax.collections), 1)
+    finally:
+        ax.remove()
+
+
+def test_plot_sphere_wireframe():
+    ax = make_3d_axis(1.0)
+    try:
+        plot_sphere(ax, wireframe=True)
+        assert_equal(len(ax.collections), 1)
+    finally:
+        ax.remove()
+
+
+def test_plot_cylinder():
+    ax = make_3d_axis(1.0)
+    try:
+        plot_cylinder(ax, wireframe=False)
+        assert_equal(len(ax.collections), 1)
+    finally:
+        ax.remove()
+
+
+def test_plot_cylinder_wireframe():
+    ax = make_3d_axis(1.0)
+    try:
+        plot_cylinder(ax, wireframe=True)
+        assert_equal(len(ax.collections), 1)
+    finally:
+        ax.remove()
+
+
+def test_plot_mesh():
+    ax = make_3d_axis(1.0)
+    try:
+        plot_mesh(ax, filename="test/test_data/cone.stl", wireframe=False)
+        assert_equal(len(ax.collections), 1)
+    finally:
+        ax.remove()
+
+
+def test_plot_mesh_wireframe():
+    ax = make_3d_axis(1.0)
+    try:
+        plot_mesh(ax, filename="test/test_data/cone.stl", wireframe=True)
+        assert_equal(len(ax.collections), 1)
+    finally:
+        ax.remove()
+
+
+def test_plot_vector():
+    ax = make_3d_axis(1.0)
+    try:
+        plot_vector(ax)
+        assert_equal(len(ax.artists), 1)
+    finally:
+        ax.remove()
+
+
+def test_plot_length_variable():
+    ax = make_3d_axis(1.0)
+    try:
+        plot_length_variable(ax)
+        assert_greater_equal(len(ax.lines), 1)
+        assert_equal(len(ax.texts), 1)
     finally:
         ax.remove()
