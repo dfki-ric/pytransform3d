b3a316394d40d2d4a388bfe59f87613159eb1a22
Alexander Fabisch <afabisch@googlemail.com>
2021-06-20 14:39:41 +0200

    Version 1.9.1: fixes missing subpackages

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 import pytransform3d
 
 
 if __name__ == "__main__":
     with open("README.md", "r") as f:
         long_description = f.read()
-    setup(name='pytransform3d',
+    setup(name="pytransform3d",
           version=pytransform3d.__version__,
           author='Alexander Fabisch',
           author_email='afabisch@googlemail.com',
@@ -23,7 +23,7 @@ if __name__ == "__main__":
               "Topic :: Scientific/Engineering :: Visualization",
           ],
           license='BSD-3-Clause',
-          packages=['pytransform3d'],
+          packages=find_packages(),
           install_requires=["numpy", "scipy", "matplotlib", "lxml",
                             "beautifulsoup4"],
           extras_require={
