a3e50b91f9ee207f111e1b6a6c0f8cd2cf2bddd5
Alexander Fabisch <afabisch@googlemail.com>
2021-08-07 19:06:58 +0200

    Add example

---
+++ examples/plots/plot_spherical_grid.py
@@ -0,0 +1,23 @@
+"""
+==============
+Spherical Grid
+==============
+
+Plot a grid in spherical coordinates with rho = 1 as Cartesian points.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pytransform3d.coordinates as pc
+from pytransform3d.plot_utils import make_3d_axis
+
+
+thetas, phis = np.meshgrid(np.linspace(0, np.pi, 11),
+                           np.linspace(-np.pi, np.pi, 21))
+rhos = np.ones_like(thetas)
+spherical_grid = np.column_stack((rhos.reshape(-1), thetas.reshape(-1), phis.reshape(-1)))
+cartesian_grid = pc.cartesian_from_spherical(spherical_grid)
+
+ax = make_3d_axis(ax_s=1, unit="m", n_ticks=6)
+ax.scatter(cartesian_grid[:, 0], cartesian_grid[:, 1], cartesian_grid[:, 2])
+ax.plot(cartesian_grid[:, 0], cartesian_grid[:, 1], cartesian_grid[:, 2])
+plt.show()
