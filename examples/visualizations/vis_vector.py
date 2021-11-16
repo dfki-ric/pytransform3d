f6b10ac9ccd94d897c1146aa7e9c36180f03ab46
Alexander Fabisch <afabisch@googlemail.com>
2021-11-16 17:46:44 +0100

    Add example with Vector3D

---
+++ examples/visualizations/vis_vector.py
@@ -0,0 +1,22 @@
+"""
+================
+Visualize Vector
+================
+"""
+print(__doc__)
+
+
+import numpy as np
+import pytransform3d.visualizer as pv
+
+
+fig = pv.figure()
+fig.plot_vector(start=np.array([0.0, 0.0, 0.0]),
+                direction=np.array([1.0, 1.0, 0.0]),
+                c=(1.0, 0.5, 0.0))
+fig.plot_transform(A2B=np.eye(4))
+fig.view_init()
+if "__file__" in globals():
+    fig.show()
+else:
+    fig.save_image("__open3d_rendered_image.jpg")
