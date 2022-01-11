c6d74b49f47b2badd969c0cdd61c4397ea869c28
Alexander Fabisch <afabisch@googlemail.com>
2022-01-11 09:06:00 +0100

    Make Arrow3D compatible with matplotlib 3.5

--- pytransform3d/plot_utils/_artists.py
+++ pytransform3d/plot_utils/_artists.py
@@ -295,3 +295,7 @@ class Arrow3D(FancyArrowPatch):
         xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, M)
         self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
         super(Arrow3D, self).draw(renderer)
+
+    def do_3d_projection(self, renderer=None):
+        # This supports both matplotlib 3.4 and 3.5
+        return 0
