a5774ecfe1b64209613186326c00f8ee6fd307c7
Alexander Fabisch <alexander.fabisch@dfki.de>
2022-05-30 14:31:54 +0200

    Replace deprecated functions

--- examples/visualizations/vis_moving_trajectory.py
+++ examples/visualizations/vis_moving_trajectory.py
@@ -10,7 +10,7 @@ print(__doc__)
 
 import numpy as np
 import pytransform3d.visualizer as pv
-from pytransform3d.rotations import matrix_from_angle, R_id
+from pytransform3d.rotations import passive_matrix_from_angle, R_id
 from pytransform3d.transformations import transform_from, concat
 
 
@@ -21,7 +21,7 @@ def update_trajectory(step, n_frames, trajectory):
     H_mod = np.eye(4)
     for i, t in enumerate(np.linspace(0, progress, len(H))):
         H0[:3, 3] = np.array([t, 0, t])
-        H_mod[:3, :3] = matrix_from_angle(2, 8 * np.pi * t)
+        H_mod[:3, :3] = passive_matrix_from_angle(2, 8 * np.pi * t)
         H[i] = concat(H0, H_mod)
 
     trajectory.set_data(H)
