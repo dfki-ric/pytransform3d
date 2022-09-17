3e8762e36ffecc5540209746e8c221cfd61a0df8
Alexander Fabisch <afabisch@googlemail.com>
2022-09-17 10:06:25 +0200

    Simplify solution

--- pytransform3d/test/test_rotations.py
+++ pytransform3d/test/test_rotations.py
@@ -1617,8 +1617,9 @@ def test_axis_angle_from_matrix_cos_angle_greater_1():
 def test_axis_angle_from_matrix_without_check():
     R = -np.eye(3)
     with warnings.catch_warnings(record=True) as w:
-        pr.axis_angle_from_matrix(R, check=False)
-    assert_equal(len(w), 0)
+        a = pr.axis_angle_from_matrix(R, check=False)
+    assert_equal(len(w), 1)
+    assert_true(all(np.isnan(a[:3])))
 
 
 def test_check_rotor():
