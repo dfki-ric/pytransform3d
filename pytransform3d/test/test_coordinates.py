a1b6f8b8a46b2e25763ebd057c9b4451d2083df7
Alexander Fabisch <alexander.fabisch@dfki.de>
2023-02-08 11:02:59 +0100

    Get rid of nose

--- pytransform3d/test/test_coordinates.py
+++ pytransform3d/test/test_coordinates.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pytransform3d.coordinates as pc
-from nose.tools import assert_less_equal
 from numpy.testing import assert_array_almost_equal
 
 
@@ -23,9 +22,8 @@ def test_convert_cartesian_cylindrical():
         p = np.array([rho, phi, z])
         q = pc.cartesian_from_cylindrical(p)
         r = pc.cylindrical_from_cartesian(q)
-        assert_less_equal(0, r[0])
-        assert_less_equal(-np.pi, r[1])
-        assert_less_equal(r[1], np.pi)
+        assert 0 <= r[0]
+        assert -np.pi <= r[1] <= np.pi
         s = pc.cartesian_from_cylindrical(r)
         assert_array_almost_equal(q, s)
 
@@ -49,11 +47,9 @@ def test_convert_cartesian_spherical():
         p = np.array([rho, theta, phi])
         q = pc.cartesian_from_spherical(p)
         r = pc.spherical_from_cartesian(q)
-        assert_less_equal(0, r[0])
-        assert_less_equal(0, r[1])
-        assert_less_equal(r[1], np.pi)
-        assert_less_equal(-np.pi, r[2])
-        assert_less_equal(r[2], np.pi)
+        assert 0 <= r[0]
+        assert 0 <= r[1] <= np.pi
+        assert -np.pi <= r[2] <= np.pi
         s = pc.cartesian_from_spherical(r)
         assert_array_almost_equal(q, s)
 
