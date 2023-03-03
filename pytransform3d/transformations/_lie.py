d2d78b4200abb8b7abf95b4543e86a951979903b
Alexander Fabisch <afabisch@googlemail.com>
2023-03-03 23:46:41 +0100

    Jacobian of SE(3)

--- pytransform3d/transformations/_lie.py
+++ pytransform3d/transformations/_lie.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 from ..rotations import cross_product_matrix
+from ._utils import check_exponential_coordinates
 
 
 def left_jacobian_SO3(omega_unit, theta):
@@ -23,3 +24,52 @@ def left_jacobian_SO3(omega_unit, theta):
     return (np.eye(3) * theta
             + (theta - math.sin(theta)) * np.dot(omega_matrix, omega_matrix)
             + (1.0 - math.cos(theta)) * omega_matrix)
+
+
+def jacobian_SE3(Stheta, check=True):
+    """Jacobian of SE(3).
+
+    Parameters
+    ----------
+    Stheta : array-like, shape (6,)
+        Exponential coordinates of transformation:
+        S * theta = (omega_1, omega_2, omega_3, v_1, v_2, v_3) * theta,
+        where S is the screw axis, the first 3 components are related to
+        rotation and the last 3 components are related to translation.
+        Theta is the rotation angle and h * theta the translation.
+
+    check : bool, optional (default: True)
+        Check if exponential coordinates are valid
+    """
+    if check:
+        Stheta = check_exponential_coordinates(Stheta)
+
+    rho = Stheta[:3]
+    theta = np.linalg.norm(rho)
+
+    phi = Stheta[:3]
+
+    Phi = cross_product_matrix(phi)
+    Rho = cross_product_matrix(rho)
+    theta2 = theta * theta
+    theta3 = theta2 * theta
+    theta4 = theta3 * theta
+    theta5 = theta4 * theta
+    Q = (0.5 * cross_product_matrix(rho)
+         + (theta - math.sin(theta)) / theta3 * (
+                 np.dot(Phi, Rho) + np.dot(Rho, Phi)
+                 + np.dot(Phi, np.dot(Rho, Phi)))
+         - (1.0 - 0.5 * theta * theta - math.cos(theta)) / theta4 * (
+                np.dot(Phi, np.dot(Phi, Rho))
+                + np.dot(Rho, np.dot(Phi, Phi))
+                - 3 * np.dot(Phi, np.dot(Rho, Phi)))
+         - 0.5 * ((((1.0 - 0.5 * theta2) - math.cos(theta)) / theta4
+                   - (theta - math.sin(theta) - theta3 / 6.0) / theta5)
+                  * (np.dot(Phi, np.dot(Rho, np.dot(Phi, Phi)))
+                     + np.dot(Phi, np.dot(Phi, np.dot(Rho, Phi)))))
+         )
+    J = left_jacobian_SO3(rho / theta, theta)
+    return np.block([
+        [J, Q],
+        [np.eye(3), J]
+    ])
