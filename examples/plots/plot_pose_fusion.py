816e46cbcb97d4f0218a58dc2cce1eeafe6643e1
Alexander Fabisch <afabisch@googlemail.com>
2023-03-06 23:54:46 +0100

    Clean up example

--- examples/plots/plot_pose_fusion.py
+++ examples/plots/plot_pose_fusion.py
@@ -1,31 +1,15 @@
 """
-====
-TODO
-====
+============
+Fuse 3 Poses
+============
+
+Each of the poses is has an associated covariance that is considered during
+the fusion.
 """
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.patches import Ellipse
 import pytransform3d.uncertainty as pu
 import pytransform3d.rotations as pr
-import pytransform3d.transformations as pt
-
-
-def fuse_poses_mc(means, covs, n_samples=10000, rng=np.random.default_rng(0)):
-    """TODO"""
-    n_poses = len(means)
-    cov_L = [np.linalg.cholesky(cov) for cov in covs]
-    pose_indices = rng.integers(0, n_poses, n_samples)
-    epsilons = rng.standard_normal((n_samples, 6))
-    samples = np.empty((n_samples, 6))
-    for n in range(n_samples):
-        samples[n] = pu.tran2vec(
-            np.dot(pu.vec2tran(
-                np.dot(cov_L[pose_indices[n]], epsilons[n])),
-                means[pose_indices[n]]))
-    mean = np.mean(samples, axis=0)
-    cov = np.cov(samples, rowvar=0, bias=True)
-    return pu.vec2tran(mean), cov
 
 
 x_true = np.array([1.0, 0.0, 0.0, 0.0, 0.0, np.pi / 6.0])
@@ -63,14 +47,8 @@ x1 = pu.tran2vec(T1)
 x2 = pu.tran2vec(T2)
 x3 = pu.tran2vec(T3)
 
-T_est, cov_est = pu.fuse_poses([T1, T2, T3], [cov1, cov2, cov3])
-T_mc, cov_mc = fuse_poses_mc([T1, T2, T3], [cov1, cov2, cov3])
-print(T_est)
-print(T_mc)
-print(np.round(cov_est, 2))
-print(np.round(cov_mc, 2))
+T_est, cov_est, V = pu.fuse_poses([T1, T2, T3], [cov1, cov2, cov3])
 x_est = pu.tran2vec(T_est)
-x_mc = pu.tran2vec(T_mc)
 
 _, axes = plt.subplots(
     nrows=6, ncols=6, sharex=True, sharey=True, squeeze=True, figsize=(10, 10))
@@ -93,12 +71,6 @@ for i in range(6):
         pu.plot_error_ellipse(
             ax, x_est[indices], cov_est[indices][:, indices],
             color="k", alpha=0.4, factors=factors)
-
-        angle, width, height = pu.to_ellipse(cov_mc[indices][:, indices], 1.0)
-        ell = Ellipse(xy=x_mc[indices], width=2.0 * width, height=2.0 * height,
-                      angle=np.degrees(angle), fill=False, edgecolor="k",
-                      linewidth=2)
-        ax.add_artist(ell)
 
         ax.set_xlim((-5, 5))
         ax.set_ylim((-5, 5))
