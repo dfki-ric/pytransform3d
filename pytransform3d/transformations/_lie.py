3b2b5229bfa58a91ef973f7845269a16d051a746
Alexander Fabisch <afabisch@googlemail.com>
2023-03-07 00:24:50 +0100

    Extract inverse left Jacobian of SO(3)

--- pytransform3d/transformations/_lie.py
+++ pytransform3d/transformations/_lie.py
@@ -23,3 +23,25 @@ def left_jacobian_SO3(omega_unit, theta):
     return (np.eye(3) * theta
             + (theta - math.sin(theta)) * np.dot(omega_matrix, omega_matrix)
             + (1.0 - math.cos(theta)) * omega_matrix)
+
+
+def left_jacobian_SO3_inv(omega_unit, theta):
+    """Inverse left Jacobian of SO(3).
+
+    Parameters
+    ----------
+    omega_unit : array, shape (3,)
+        Axis of rotation.
+
+    theta : float
+        Angle of rotation.
+
+    Returns
+    -------
+    J_inv : array, shape (3, 3)
+        Inverse left Jacobian of SO(3).
+    """
+    omega_matrix = cross_product_matrix(omega_unit)
+    return (np.eye(3) / theta - 0.5 * omega_matrix
+            + (1.0 / theta - 0.5 / np.tan(theta / 2.0))
+            * np.dot(omega_matrix, omega_matrix))
