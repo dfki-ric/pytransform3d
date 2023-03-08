6895854b874bc17a3db10af8e1483f34095efc3a
Alexander Fabisch <afabisch@googlemail.com>
2023-03-08 23:26:10 +0100

    Better example

--- examples/plots/plot_pose_fusion.py
+++ examples/plots/plot_pose_fusion.py
@@ -11,6 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pytransform3d.uncertainty as pu
 import pytransform3d.rotations as pr
+import pytransform3d.transformations as pt
 
 
 x_true = np.array([1.0, 0.0, 0.0, 0.0, 0.0, np.pi / 6.0])
@@ -22,27 +23,9 @@ cov3 = alpha * np.diag([0.2, 0.1, 0.1, 1.0, 1.0, 5.0])
 
 rng = np.random.default_rng(0)
 
-T1 = np.array([
-    [0.8573, -0.2854, 0.4285, 3.5368],
-    [-0.1113, 0.7098, 0.6956, -3.5165],
-    [-0.5026, -0.6440, 0.5767, -0.9112],
-    [0.0, 0.0, 0.0, 1.0000]
-])
-T1[:3, :3] = pr.norm_matrix(T1[:3, :3])
-T2 = np.array([
-    [0.5441, -0.6105, 0.5755, -1.0935],
-    [0.8276, 0.5032, -0.2487, 5.5992],
-    [-0.1377, 0.6116, 0.7791, 0.2690],
-    [0.0, 0.0, 0.0, 1.0000]
-])
-T2[:3, :3] = pr.norm_matrix(T2[:3, :3])
-T3 = np.array([
-    [-0.0211, -0.7869, 0.6167, -3.0968],
-    [-0.2293, 0.6042, 0.7631, 2.0868],
-    [-0.9731, -0.1254, -0.1932, 2.0239],
-    [0.0, 0.0, 0.0, 1.0000]
-])
-T3[:3, :3] = pr.norm_matrix(T3[:3, :3])
+T1 = pt.random_transform(rng, T_true, cov1)
+T2 = pt.random_transform(rng, T_true, cov2)
+T3 = pt.random_transform(rng, T_true, cov3)
 
 x1 = pu.tran2vec(T1)
 x2 = pu.tran2vec(T2)
