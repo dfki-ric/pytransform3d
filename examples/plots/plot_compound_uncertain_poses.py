90b1d8d5110ad039fdb5b33a32cffbe5e2068ebe
Alexander Fabisch <afabisch@googlemail.com>
2023-03-11 21:12:08 +0100

    Plot ellipsoid in 2d

--- examples/plots/plot_compound_uncertain_poses.py
+++ examples/plots/plot_compound_uncertain_poses.py
@@ -8,7 +8,6 @@ Each of the poses is has an associated covariance that is considered.
 import numpy as np
 import matplotlib.pyplot as plt
 import pytransform3d.uncertainty as pu
-import pytransform3d.trajectories as ptr
 
 
 rng = np.random.default_rng(0)
@@ -17,16 +16,16 @@ cov_pose = np.dot(cov_pose_chol, cov_pose_chol.T)
 velocity_vector = np.array([0, 0, 0, 1.0, 0, 0])
 T_vel = pu.transform_from_vector(velocity_vector)
 n_steps = 100
-n_mc_samples = 100
+n_mc_samples = 1000
 plot_dimensions = np.array([3, 4], dtype=int)
 
-T = np.eye(4)
+T_est = np.eye(4)
 path = np.zeros((n_steps + 1, 6))
-path[0] = pu.vector_from_transform(T)
-cov = cov_pose
+path[0] = pu.vector_from_transform(T_est)
+cov_est = cov_pose
 for t in range(n_steps):
-    T, cov = pu.compund_poses(T_vel, cov_pose, T, cov)
-    path[t + 1] = pu.vector_from_transform(T)
+    T_est, cov_est = pu.compund_poses(T_vel, cov_pose, T_est, cov_est)
+    path[t + 1] = pu.vector_from_transform(T_est)
 
 T = np.eye(4)
 mc_path = np.zeros((n_steps + 1, n_mc_samples, 4, 4))
@@ -47,21 +46,23 @@ plt.plot(
 plt.scatter(
     mc_path_vec[-1, :, 0],
     mc_path_vec[-1, :, 1], s=5, c="b")
+
 plt.plot(
     path[:, plot_dimensions[0]], path[:, plot_dimensions[1]], lw=3, color="k")
 
-factors = [1.65, 1.96, 2.58]
-pu.plot_error_ellipse(
-    plt.gca(), path[-1, plot_dimensions],
-    cov[plot_dimensions][:, plot_dimensions],
-    color="b", alpha=0.4, factors=factors)
+pu.plot_projected_ellipse(
+    plt.gca(), T_est, cov_est, np.array([0, 1], dtype=int), color="g",
+    factor=3.0)
 
 mean_mc = np.mean(mc_path_vec[-1, :], axis=0)
 cov_mc = np.cov(mc_path_vec[-1, :], rowvar=False)
 
+factors = [1.65, 1.96, 2.58]
 pu.plot_error_ellipse(
     plt.gca(), mean_mc, cov_mc, color="r", alpha=0.4, factors=factors)
 
 plt.xlim((-5, 105))
-plt.ylim((-45, 45))
+plt.ylim((-50, 50))
+plt.xlabel("x")
+plt.ylabel("y")
 plt.show()
