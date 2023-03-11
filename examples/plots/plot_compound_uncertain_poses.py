08281cea5a94b4cd573a3d28fc619e9b7eda63fd
Alexander Fabisch <afabisch@googlemail.com>
2023-03-11 19:33:59 +0100

    Clean up

--- examples/plots/plot_compound_uncertain_poses.py
+++ examples/plots/plot_compound_uncertain_poses.py
@@ -16,25 +16,24 @@ cov_pose = np.dot(cov_pose_chol, cov_pose_chol.T)
 velocity_vector = np.array([0, 0, 0, 1.0, 0, 0])
 T_vel = pu.transform_from_vector(velocity_vector)
 n_steps = 100
-n_mc_samples = 1000
+n_mc_samples = 100
 plot_dimensions = np.array([3, 4], dtype=int)
 
 T = np.eye(4)
-path = np.zeros((n_steps + 1, len(plot_dimensions)))
-path[0] = pu.vector_from_transform(T)[plot_dimensions]
-mc_path = np.zeros((n_steps + 1, n_mc_samples, 4, 4))
-mc_path[0, :] = T
+path = np.zeros((n_steps + 1, 6))
+path[0] = pu.vector_from_transform(T)
 cov = cov_pose
-e = np.zeros(6)
 for t in range(n_steps):
     T, cov = pu.compund_poses(T_vel, cov_pose, T, cov)
-    e = pu.vector_from_transform(T)
-    path[t + 1] = e[plot_dimensions]
+    path[t + 1] = pu.vector_from_transform(T)
 
+T = np.eye(4)
+mc_path = np.zeros((n_steps + 1, n_mc_samples, 4, 4))
+mc_path[0, :] = T
+for t in range(n_steps):
     diff_samples = cov_pose_chol.dot(rng.standard_normal(size=(6, n_mc_samples))).T
     for i in range(n_mc_samples):
         mc_path[t + 1, i] = pu.transform_from_vector(diff_samples[i]).dot(T_vel).dot(mc_path[t, i])
-
 mc_path_vec = np.zeros((n_steps, n_mc_samples, 6))
 for t in range(n_steps):
     for i in range(n_mc_samples):
@@ -43,11 +42,13 @@ for t in range(n_steps):
 plt.plot(
     mc_path_vec[:, :, plot_dimensions[0]],
     mc_path_vec[:, :, plot_dimensions[1]], lw=1, c="b", alpha=0.1)
-plt.plot(path[:, 0], path[:, 1], lw=3, color="k")
+plt.plot(
+    path[:, plot_dimensions[0]], path[:, plot_dimensions[1]], lw=3, color="k")
 
 factors = [1.65, 1.96, 2.58]
 pu.plot_error_ellipse(
-    plt.gca(), e[plot_dimensions], cov[plot_dimensions][:, plot_dimensions],
+    plt.gca(), path[-1, plot_dimensions],
+    cov[plot_dimensions][:, plot_dimensions],
     color="b", alpha=0.4, factors=factors)
 
 mean_mc = np.mean(mc_path_vec[-1, :, plot_dimensions], axis=1)
