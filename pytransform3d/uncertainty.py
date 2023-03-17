ebbf0378d7a2c50cf4ce714c9850fa947957eb2a
Alexander Fabisch <alexander.fabisch@dfki.de>
2023-03-17 09:35:36 +0100

    Resolve ambiguity

--- pytransform3d/uncertainty.py
+++ pytransform3d/uncertainty.py
@@ -1,8 +1,10 @@
 import math
 import numpy as np
 import scipy as sp
-from .transformations import (invert_transform, check_exponential_coordinates,
-                              adjoint_from_transform)
+from .transformations import (
+    invert_transform, check_exponential_coordinates, adjoint_from_transform,
+    transform_from_exponential_coordinates,
+    exponential_coordinates_from_transform)
 from .rotations import (cross_product_matrix, compact_axis_angle_from_matrix,
                         matrix_from_compact_axis_angle, left_jacobian_SO3,
                         left_jacobian_SO3_inv)
@@ -44,19 +46,19 @@ def fuse_poses(means, covs):
         LHS = np.zeros((6, 6))
         RHS = np.zeros(6)
         for k in range(n_poses):
-            x_ik = vector_from_transform(np.dot(mean, invert_transform(means[k])))
+            x_ik = exponential_coordinates_from_transform(np.dot(mean, invert_transform(means[k])))
             J_inv = jacobian_SE3_inv(x_ik)
             J_invT_S = np.dot(J_inv.T, covs_inv[k])
             LHS += np.dot(J_invT_S, J_inv)
             RHS += np.dot(J_invT_S, x_ik)
         x_i = np.linalg.solve(-LHS, RHS)
-        mean = np.dot(transform_from_vector(x_i), mean)
+        mean = np.dot(transform_from_exponential_coordinates(x_i), mean)
 
     cov = np.linalg.inv(LHS)
 
     V = 0.0
     for k in range(n_poses):
-        x_ik = vector_from_transform(np.dot(mean, invert_transform(means[k])))
+        x_ik = exponential_coordinates_from_transform(np.dot(mean, invert_transform(means[k])))
         V += 0.5 * np.dot(x_ik, np.dot(covs_inv[k], x_ik))
     return mean, cov, V
 
@@ -240,26 +242,6 @@ def _Q(Stheta):
     return Q
 
 
-def vector_from_transform(T):
-    C = T[:3, :3]
-    r = T[:3, 3]
-    phi = compact_axis_angle_from_matrix(C)
-    J_inv = left_jacobian_SO3_inv(phi)
-    rho = np.dot(J_inv, r)
-    return np.hstack((phi, rho))
-
-
-def transform_from_vector(p):
-    rho = p[3:]
-    phi = p[:3]
-    C = matrix_from_compact_axis_angle(phi)
-    J = left_jacobian_SO3(phi)
-    T = np.eye(4)
-    T[:3, :3] = C
-    T[:3, 3] = np.dot(J, rho)
-    return T
-
-
 def to_ellipse(cov, factor=1.0):
     """Compute error ellipse.
 
@@ -371,7 +353,7 @@ def plot_projected_ellipse(
         P = (w[ind1[n]] * vecs[np.newaxis, :, ind1[n]] * S[:, np.newaxis]
              + w[ind2[n]] * vecs[np.newaxis, :, ind2[n]] * C[:, np.newaxis])
         for m in range(n_steps):
-            T = transform_from_vector(P[m]).dot(mean)
+            T = transform_from_exponential_coordinates(P[m]).dot(mean)
             r = T[:3, :3].T.dot(T[:3, 3])
             clines[:, m] = r[dimensions]
         ax.plot(clines[0], clines[1], color=color, alpha=alpha)
