59e36f54af859d079cf4fb50e5bc4c0d4af9bfb8
Alexander Fabisch <afabisch@googlemail.com>
2023-03-18 11:37:03 +0100

    Fix examples

--- examples/plots/plot_compound_uncertain_poses.py
+++ examples/plots/plot_compound_uncertain_poses.py
@@ -4,9 +4,15 @@ Compound Uncertain Poses
 ========================
 
 Each of the poses is has an associated covariance that is considered.
+
+This example is from
+
+Barfoot, Furgale: Associating Uncertainty With Three-Dimensional Poses for Use
+in Estimation Problems, http://ncfrn.mcgill.ca/members/pubs/barfoot_tro14.pdf
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import pytransform3d.transformations as pt
 import pytransform3d.uncertainty as pu
 
 
@@ -14,18 +20,18 @@ rng = np.random.default_rng(0)
 cov_pose_chol = np.diag([0, 0, 0.03, 0, 0, 0])
 cov_pose = np.dot(cov_pose_chol, cov_pose_chol.T)
 velocity_vector = np.array([0, 0, 0, 1.0, 0, 0])
-T_vel = pu.transform_from_vector(velocity_vector)
+T_vel = pt.transform_from_exponential_coordinates(velocity_vector)
 n_steps = 100
 n_mc_samples = 1000
 plot_dimensions = np.array([3, 4], dtype=int)
 
 T_est = np.eye(4)
 path = np.zeros((n_steps + 1, 6))
-path[0] = pu.vector_from_transform(T_est)
+path[0] = pt.exponential_coordinates_from_transform(T_est)
 cov_est = cov_pose
 for t in range(n_steps):
     T_est, cov_est = pu.compund_poses(T_vel, cov_pose, T_est, cov_est)
-    path[t + 1] = pu.vector_from_transform(T_est)
+    path[t + 1] = pt.exponential_coordinates_from_transform(T_est)
 
 T = np.eye(4)
 mc_path = np.zeros((n_steps + 1, n_mc_samples, 4, 4))
@@ -33,7 +39,7 @@ mc_path[0, :] = T
 for t in range(n_steps):
     diff_samples = cov_pose_chol.dot(rng.standard_normal(size=(6, n_mc_samples))).T
     for i in range(n_mc_samples):
-        mc_path[t + 1, i] = pu.transform_from_vector(diff_samples[i]).dot(T_vel).dot(mc_path[t, i])
+        mc_path[t + 1, i] = pt.transform_from_exponential_coordinates(diff_samples[i]).dot(T_vel).dot(mc_path[t, i])
 # Plot the random samples' trajectory lines (in a frame attached to the start)
 mc_path_vec = np.zeros((n_steps, n_mc_samples, 2))
 for t in range(n_steps):
