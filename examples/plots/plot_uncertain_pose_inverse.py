6c9631cb14abf54c4d0ca43bd21bbd2080f8af8c
Alexander Fabisch <afabisch@googlemail.com>
2023-03-19 10:50:43 +0100

    Covariance with correlations

--- examples/plots/plot_uncertain_pose_inverse.py
+++ examples/plots/plot_uncertain_pose_inverse.py
@@ -11,26 +11,37 @@ import pytransform3d.transformations as pt
 import pytransform3d.trajectories as ptr
 
 
+n_mc_samples = 100
+rng = np.random.default_rng(1)
+alpha = 2.0
+
 A2B = pt.transform_from(
     R=pr.matrix_from_euler([1.5, 0.5, 1.3], 0, 1, 2, True),
-    p=[5.0, 1.0, 2.0]
+    p=[10.0, 1.0, -5.0]
 )
-alpha = 2.0
-cov_A2B = alpha * np.diag([0.1, 0.2, 0.1, 2.0, 1.0, 1.0])
+variances = alpha * np.array([0.1, 0.2, 0.1, 2.0, 1.0, 1.0])
+std_devs = np.sqrt(variances)
+correlations = np.array([
+    [0.5, 0, 0, 0, 0, 0],
+    [0.1, 0.5, 0, 0, 0, 0],
+    [-0.3, 0.1, 0.5, 0, 0, 0],
+    [-0.1, 0.2, 0.3, 0.5, 0, 0],
+    [-0.3, -0.1, -0.2, 0.3, 0.5, 0],
+    [-0.1, 0.1, 0.2, 0.1, 0.3, 0.5]
+])
+correlations += correlations.T
+cov_A2B = correlations * np.outer(std_devs, std_devs) / 2
 x_A2B = pt.exponential_coordinates_from_transform(A2B)
+samples_A2B = ptr.exponential_coordinates_from_transforms(
+    [pt.random_transform(rng, A2B, cov_A2B) for _ in range(n_mc_samples)])
+
 B2A, cov_B2A = pu.invert_uncertain_transform(A2B, cov_A2B)
 x_B2A = pt.exponential_coordinates_from_transform(B2A)
-
-n_samples = 100
-rng = np.random.default_rng(1)
-samples_A2B = ptr.exponential_coordinates_from_transforms(
-    [pt.random_transform(rng, A2B, cov_A2B) for _ in range(n_samples)])
 samples_B2A = ptr.exponential_coordinates_from_transforms(
-    [pt.random_transform(rng, B2A, cov_B2A) for _ in range(n_samples)])
+    [pt.random_transform(rng, B2A, cov_B2A) for _ in range(n_mc_samples)])
 
 factors = [1.65, 1.96, 2.58]
-_, axes = plt.subplots(
-    nrows=6, ncols=6, sharex=True, sharey=True, squeeze=True, figsize=(10, 10))
+_, axes = plt.subplots(nrows=6, ncols=6, squeeze=True, figsize=(10, 10))
 for i in range(6):
     for j in range(6):
         if i == j:
