72a6387b6beea68f61c047296dba005462c7c94c
Alexander Fabisch <afabisch@googlemail.com>
2023-03-19 18:38:02 +0100

    Make example gallery-compatible

--- examples/plots/plot_spheres.py
+++ examples/plots/plot_spheres.py
@@ -1,43 +1,38 @@
+"""
+=====================
+Plot Multiple Spheres
+=====================
+
+Benchmarks plotting of multiple spheres at once and compares it to plotting
+each sphere individually.
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 from pytransform3d.plot_utils import plot_sphere, plot_spheres
 import time
 
-time_single = []
-time_multi = []
-speedup = []
+
+n_spheres = 50
+random_state = np.random.default_rng(0)
+P = 2 * random_state.random((n_spheres, 3)) - 1
+radii = random_state.random(n_spheres) / 2
+colors = random_state.random((n_spheres, 3))
+alphas = random_state.random(n_spheres)
+
+start = time.time()
+plot_spheres(p=P, radius=radii, color=colors, alpha=alphas, wireframe=False)
+end = time.time()
+time_multi = end - start
+
+start = time.time()
+for p, radius, color, alpha in zip(P, radii, colors, alphas):
+    plot_sphere(p=p, radius=radius, color=color, alpha=alpha, wireframe=False)
+end = time.time()
+time_single = end - start
+
+speedup = time_single / time_multi
 
 print("n_spheres", "single", "\t", "multi", "\t", "speedup", sep="\t")
+print(n_spheres, "", time_single, time_multi, speedup, sep="\t")
 
-for n_spheres in range(51):
-    random_state = np.random.RandomState(0)
-    P = 2 * random_state.rand(n_spheres, 3) - 1
-    radii = random_state.rand(n_spheres) / 2
-    colors = random_state.rand(n_spheres, 3)
-    alphas = random_state.rand(n_spheres)
-
-    for multi in range(2):
-        start = time.time()
-
-        if multi:
-            plot_spheres(p=P, radius=radii, color=colors, alpha=alphas, wireframe=False)
-
-        else:
-            for p, radius, color, alpha in zip(P, radii, colors, alphas):
-                plot_sphere(p=p, radius=radius, color=color, alpha=alpha, wireframe=False)
-
-        end = time.time()
-        diff = end - start
-
-        if multi:
-            time_multi.append(diff)
-
-        else:
-            time_single.append(diff)
-
-    speedup.append(time_single[-1] / time_multi[-1])
-    print(n_spheres, "", time_single[-1], time_multi[-1], speedup[-1], sep="\t")
-
-print("mean", "", np.mean(time_single), np.mean(time_multi), np.mean(speedup), sep="\t")
-
-# plt.show()
+plt.show()
