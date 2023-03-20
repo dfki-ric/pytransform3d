02c8bab413c240eafd0f2c0ce8b5e497331098c1
Alexander Fabisch <afabisch@googlemail.com>
2023-03-21 00:01:46 +0100

    Iterative approximation of mean

--- examples/plots/plot_pose_sampling.py
+++ examples/plots/plot_pose_sampling.py
@@ -11,7 +11,8 @@ mean = pt.transform_from(R=np.eye(3), p=np.array([0.0, 0.0, 0.5]))
 cov = np.diag([0.001, 0.001, 0.5, 0.001, 0.001, 0.001])
 rng = np.random.default_rng(0)
 ax = None
-for _ in range(200):
-    pose = pt.random_transform(rng=rng, mean=mean, cov=cov)
+poses = np.array([
+    pt.random_transform(rng=rng, mean=mean, cov=cov) for _ in range(1000)])
+for pose in poses:
     ax = pt.plot_transform(ax=ax, A2B=pose, s=0.3)
 plt.show()
