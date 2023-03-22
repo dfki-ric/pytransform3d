ce0c4a01a66e293113c44492ff0fbd8bf5895e1d
Alexander Fabisch <alexander.fabisch@dfki.de>
2023-03-22 13:51:43 +0100

    Clean up pose inversion example

--- examples/plots/plot_uncertain_pose_inverse.py
+++ examples/plots/plot_uncertain_pose_inverse.py
@@ -2,6 +2,11 @@
 ============================
 Inverse of an Uncertain Pose
 ============================
+
+We sample from the original pose distribution and from the inverse
+distribution. Samples are then projected to all 2D planes and plotted.
+The color indicates the pose distribution. Green is the original
+distribution and red is the inverse.
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,13 +16,13 @@ import pytransform3d.transformations as pt
 import pytransform3d.trajectories as ptr
 
 
-n_mc_samples = 100
+n_mc_samples = 1000
 rng = np.random.default_rng(1)
 alpha = 2.0
 
 A2B = pt.transform_from(
     R=pr.matrix_from_euler([1.5, 0.5, 1.3], 0, 1, 2, True),
-    p=[10.0, 1.0, -5.0]
+    p=[10.0, -7.0, -5.0]
 )
 variances = alpha * np.array([0.1, 0.2, 0.1, 2.0, 1.0, 1.0])
 std_devs = np.sqrt(variances)
@@ -30,7 +35,7 @@ correlations = np.array([
     [-0.1, 0.1, 0.2, 0.1, 0.3, 0.5]
 ])
 correlations += correlations.T
-cov_A2B = correlations * np.outer(std_devs, std_devs) / 2
+cov_A2B = correlations * np.outer(std_devs, std_devs)
 x_A2B = pt.exponential_coordinates_from_transform(A2B)
 samples_A2B = ptr.exponential_coordinates_from_transforms(
     [pt.random_transform(rng, A2B, cov_A2B) for _ in range(n_mc_samples)])
@@ -40,7 +45,6 @@ x_B2A = pt.exponential_coordinates_from_transform(B2A)
 samples_B2A = ptr.exponential_coordinates_from_transforms(
     [pt.random_transform(rng, B2A, cov_B2A) for _ in range(n_mc_samples)])
 
-factors = [1.65, 1.96, 2.58]
 _, axes = plt.subplots(nrows=6, ncols=6, squeeze=True, figsize=(10, 10))
 for i in range(6):
     for j in range(6):
@@ -51,16 +55,9 @@ for i in range(6):
         ax = axes[i][j]
 
         ax.scatter(
-            samples_A2B[:, i], samples_A2B[:, j], color="g", s=5, alpha=0.3)
-        pu.plot_error_ellipse(
-            ax, x_A2B[indices], cov_A2B[indices][:, indices],
-            color="g", alpha=0.3, factors=factors)
-
+            samples_A2B[:, i], samples_A2B[:, j], color="g", s=1, alpha=0.3)
         ax.scatter(
-            samples_B2A[:, i], samples_B2A[:, j], color="r", s=5, alpha=0.3)
-        pu.plot_error_ellipse(
-            ax, x_B2A[indices], cov_B2A[indices][:, indices],
-            color="r", alpha=0.3, factors=factors)
+            samples_B2A[:, i], samples_B2A[:, j], color="r", s=1, alpha=0.3)
 
         ax.scatter(0, 0, color="k")
 
