62de29c14192ff5c4a762ff962e201209ec5b9f4
Alexander Fabisch <afabisch@googlemail.com>
2023-03-22 22:06:19 +0100

    PEP8

--- examples/plots/plot_pose_sampling.py
+++ examples/plots/plot_pose_sampling.py
@@ -7,6 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pytransform3d.transformations as pt
 
+
 mean = pt.transform_from(R=np.eye(3), p=np.array([0.0, 0.0, 0.5]))
 cov = np.diag([0.001, 0.001, 0.5, 0.001, 0.001, 0.001])
 rng = np.random.default_rng(0)
