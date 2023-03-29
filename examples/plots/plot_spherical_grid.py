da13f931811f8618f8b2ae68f44fe5e02ae8d335
Alexander Fabisch <alexander.fabisch@dfki.de>
2023-03-29 09:59:50 +0200

    PEP8

--- examples/plots/plot_spherical_grid.py
+++ examples/plots/plot_spherical_grid.py
@@ -17,7 +17,8 @@ from pytransform3d.plot_utils import make_3d_axis
 thetas, phis = np.meshgrid(np.linspace(0, np.pi, 11),
                            np.linspace(-np.pi, np.pi, 21))
 rhos = np.ones_like(thetas)
-spherical_grid = np.column_stack((rhos.reshape(-1), thetas.reshape(-1), phis.reshape(-1)))
+spherical_grid = np.column_stack((
+    rhos.reshape(-1), thetas.reshape(-1), phis.reshape(-1)))
 cartesian_grid = pc.cartesian_from_spherical(spherical_grid)
 
 ax = make_3d_axis(ax_s=1, unit="m", n_ticks=6)
