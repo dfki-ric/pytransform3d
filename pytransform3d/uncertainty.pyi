8f7e5fd6c6517a7e07c155ed4584d92d63b48579
Alexander Fabisch <afabisch@googlemail.com>
2023-04-07 21:48:28 +0200

    Correct signature in stub file

--- pytransform3d/uncertainty.pyi
+++ pytransform3d/uncertainty.pyi
@@ -19,8 +19,8 @@ def concat_globally_uncertain_transforms(
 
 
 def concat_locally_uncertain_transforms(
-        mean_A2B: npt.ArrayLike, cov_A2B: npt.ArrayLike,
-        mean_B2C: npt.ArrayLike, cov_B2C: npt.ArrayLike) -> np.ndarray: ...
+        mean_A2B: npt.ArrayLike, mean_B2C: npt.ArrayLike, cov_A: npt.ArrayLike,
+        cov_B: npt.ArrayLike) -> np.ndarray: ...
 
 
 def pose_fusion(means: npt.ArrayLike, covs: npt.ArrayLike) -> np.ndarray: ...
