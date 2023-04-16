da34571b9b5381e72b7d0cdf3ec75042b1abd052
Alexander Fabisch <afabisch@googlemail.com>
2023-04-16 10:27:31 +0200

    Plot cone

--- pytransform3d/geometry.py
+++ pytransform3d/geometry.py
@@ -118,3 +118,23 @@ class Cone(GeometricShape):
         super(Cone, self).__init__(pose)
         self.height = height
         self.radius = radius
+
+    def surface(self, n_steps):
+        phi, theta = np.mgrid[0.0:np.pi:n_steps * 1j, 0.0:2.0 * np.pi:n_steps * 1j]
+        x = self.radius * np.sin(phi) * np.cos(theta)
+        y = self.radius * np.sin(phi) * np.sin(theta)
+        z = np.zeros_like(phi)
+        x[len(x) // 2:] = 0.0
+        y[len(y) // 2:] = 0.0
+        z[len(z) // 2:] += self.height
+
+        shape = x.shape
+
+        P = np.column_stack((x.reshape(-1), y.reshape(-1), z.reshape(-1)))
+        P = transform(self.pose, vectors_to_points(P))[:, :3]
+
+        x = P[:, 0].reshape(*shape)
+        y = P[:, 1].reshape(*shape)
+        z = P[:, 2].reshape(*shape)
+
+        return x, y, z
