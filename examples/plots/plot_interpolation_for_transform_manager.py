2e189bf1df0e0b199c508c84a28d93f639da11fe
Marat Kopytjuk <kopytjuk@gmail.com>
2023-05-23 20:12:38 +0200

    run black on interpolation example

--- examples/plots/plot_interpolation_for_transform_manager.py
+++ examples/plots/plot_interpolation_for_transform_manager.py
@@ -1,33 +1,34 @@
 """
-======================
+==================================
 Managing Transformations over Time
-======================
+==================================
 
-In this example, given two tranformation trajectories, we will interpolate both 
+In this example, given two tranformation trajectories, we will interpolate both
 and use the transform manager for the target timestep.
 """
 import numpy as np
-import matplotlib.pyplot as plt
+
 from pytransform3d import rotations as pr
 from pytransform3d import transformations as pt
 from pytransform3d.transform_manager import TransformManager
 
 
-def create_sinusoidal_movement(duration_sec, sample_period, x_velocity, y_start_offset, start_time):
-    """Create a planar (z=0) sinusoidal movement around x-axis.
-    """
+def create_sinusoidal_movement(
+    duration_sec, sample_period, x_velocity, y_start_offset, start_time
+):
+    """Create a planar (z=0) sinusoidal movement around x-axis."""
 
     time_arr = np.arange(0, duration_sec, sample_period) + start_time
     N = len(time_arr)
-    x_arr = np.linspace(0, x_velocity*duration_sec, N) 
+    x_arr = np.linspace(0, x_velocity * duration_sec, N)
 
-    spatial_freq = 1/5  # 1 sinus per 5m
-    y_arr = np.sin(2*np.pi*spatial_freq*x_arr)
+    spatial_freq = 1 / 5  # 1 sinus per 5m
+    y_arr = np.sin(2 * np.pi * spatial_freq * x_arr)
     y_arr += y_start_offset
-    
-    dydx_arr = np.cos(2*np.pi*spatial_freq*x_arr)
+
+    dydx_arr = np.cos(2 * np.pi * spatial_freq * x_arr)
     yaw_arr = np.arctan2(dydx_arr, np.ones_like(dydx_arr))
-    
+
     pq_arr = list()
     for i in range(N):
         R = pr.active_matrix_from_extrinsic_euler_zyx([yaw_arr[i], 0, 0])
@@ -42,10 +43,13 @@ def create_sinusoidal_movement(duration_sec, sample_period, x_velocity, y_start_
 duration = 10.0  # [s]
 sample_period = 0.5  # [s]
 velocity_x = 1  # [m/s]
-time_W2A, pq_arr_W2A = create_sinusoidal_movement(duration, sample_period,
-                                              velocity_x, y_start_offset=0.0, start_time=0.1)
-time_W2B, pq_arr_W2B = create_sinusoidal_movement(duration, sample_period,
-                                              velocity_x, y_start_offset=2.0, start_time=0.35)
+time_W2A, pq_arr_W2A = create_sinusoidal_movement(
+    duration, sample_period, velocity_x, y_start_offset=0.0, start_time=0.1
+)
+time_W2B, pq_arr_W2B = create_sinusoidal_movement(
+    duration, sample_period, velocity_x, y_start_offset=2.0, start_time=0.35
+)
+
 
 def interpolate_pq(query_time, t_arr, pq_array):
     """Interpolate a transformation trajectory at a target time.
@@ -57,13 +61,14 @@ def interpolate_pq(query_time, t_arr, pq_array):
     t_arr : array-like, shape (N,)
         Timesteps from the transformation trajectory
     pq_array : array-like, shape (N,7)
-        Transformation trajectory with each row representing 
+        Transformation trajectory with each row representing
         position and orientation quaternion: (x, y, z, qw, qx, qy, qz)
 
     Returns
     -------
     pq : array-like, shape (7,)
-        Interpolated position and orientation quaternion: (x, y, z, qw, qx, qy, qz)
+        Interpolated position and orientation quaternion:
+        (x, y, z, qw, qx, qy, qz)
     """
 
     idx_timestep_earlier_wrt_query_time = np.argmax(t_arr >= query_time) - 1
@@ -77,10 +82,11 @@ def interpolate_pq(query_time, t_arr, pq_array):
     dq_next = pt.dual_quaternion_from_pq(pq_next)
 
     # since sclerp works with relative (0-1) positions
-    rel_delta_t = (query_time - t_prev)/(t_next-t_prev)
+    rel_delta_t = (query_time - t_prev) / (t_next - t_prev)
     dq_interpolated = pt.dual_quaternion_sclerp(dq_prev, dq_next, rel_delta_t)
 
     return pt.pq_from_dual_quaternion(dq_interpolated)
+
 
 query_time = 5.0
 pq_W2A = interpolate_pq(query_time, time_W2A, pq_arr_W2A)
@@ -93,7 +99,8 @@ tm.add_transform("world", "B", pt.transform_from_pq(pq_W2B))
 
 query_transform_A2B = tm.get_transform("A", "B")
 
-origin_of_B_in_A = pt.transform(query_transform_A2B, pt.vector_to_point([0, 0, 0]))
+origin_of_B_in_A = pt.transform(query_transform_A2B,
+                                pt.vector_to_point([0, 0, 0]))
 origin_of_B_in_A = origin_of_B_in_A[:-1]
 
 assert origin_of_B_in_A[0] < 0  # x
