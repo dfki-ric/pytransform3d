eb58f231635df8f4bf14f889f0ec7ae9d645c943
Alexander Fabisch <afabisch@googlemail.com>
2023-06-29 10:57:32 +0200

    Add unit test for unit sphere grid

---
+++ pytransform3d/test/test_geometry.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+import pytransform3d.geometry as pg
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+
+
+def test_unit_sphere():
+    x, y, z = pg.unit_sphere_surface_grid(10)
+    assert_array_equal(x.shape, (10, 10))
+    assert_array_equal(y.shape, (10, 10))
+    assert_array_equal(z.shape, (10, 10))
+
+    P = np.column_stack((x.reshape(-1), y.reshape(-1), z.reshape(-1)))
+    norms = np.linalg.norm(P, axis=1)
+    assert_array_almost_equal(norms, np.ones_like(norms))
