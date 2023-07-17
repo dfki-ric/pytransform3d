c0c248014d5cb4dc1ad796e505182ea48789efd6
Marat Kopytjuk <kopytjuk@gmail.com>
2023-07-17 17:54:27 +0200

    fix rebase

--- pytransform3d/transform_manager/_transform_manager.pyi
+++ pytransform3d/transform_manager/_transform_manager.pyi
@@ -2,73 +2,10 @@ from typing import Dict, Tuple, List, Union, Set, Hashable, Any
 import numpy as np
 import numpy.typing as npt
 from mpl_toolkits.mplot3d import Axes3D
-<<<<<<< HEAD:pytransform3d/transform_manager/_transform_manager.pyi
 from ._transform_graph_base import TransformGraphBase
 
 
 PYDOT_AVAILABLE: bool
-=======
-from typing import Dict, Tuple, List, Union, Set, Hashable, Any, Protocol, runtime_checkable
-
-
-class TransformBase(abc.ABC):
-    
-    @abc.abstractmethod
-    def get_matrix(self, time: float):
-        ...
-
-    @abc.abstractmethod
-    def invert(self) -> "TransformBase":
-        """Implements invert on a generic transformation"""
-        ... 
-
-    @abc.abstractmethod
-    def concat(self, other: TransformBase) -> "TransformBase":
-        """Implements concat on a transform with potentially different time base"""
-        ...
-
-
-class TransformGraphBase(abc.ABC):
-    strict_check: bool
-    check: bool
-    nodes: List[Hashable]
-    transforms: Dict[Tuple[Hashable, Hashable], TransformBase]
-    i: List[int]
-    j: List[int]
-    transform_to_ij_index = Dict[Tuple[Hashable, Hashable], int]
-    connections: sp.csr_matrix
-    dist: np.ndarray
-    predecessors: np.ndarray
-    _cached_shortest_paths: Dict[Tuple[int, int], List[Hashable]]
-
-    def has_frame(self, frame: Hashable) -> bool: ...
-
-    def add_transform(self, from_frame: Hashable, to_frame: Hashable,
-                      A2B: Any) -> "TransformGraphBase": ...
-
-    def _check_transform(self, A2B: Any) -> Any: ...
-
-    def _recompute_shortest_path(self): ...
-
-    def remove_transform(
-            self, from_frame: Hashable,
-            to_frame: Hashable) -> "TransformManager": ...
-
-    def get_transform(
-            self, from_frame: Hashable, to_frame: Hashable) -> Any: ...
-
-    @abc.abstractmethod
-    def _invert_transform(self, A2B: Any) -> Any: ...
-
-    def _shortest_path(self, i: int, j: int) -> List[Hashable]: ...
-
-    @abc.abstractmethod
-    def _path_transform(self, path: List[Hashable]) -> Any: ...
-
-    def connected_components(self) -> int: ...
-
-    def check_consistency(self) -> bool: ...
->>>>>>> 2665b8df (try to implement TemporalTranformManager):pytransform3d/transform_manager.pyi
 
 
 class TransformManager(TransformGraphBase):
