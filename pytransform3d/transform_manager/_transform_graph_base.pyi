b9233d4d2249b6da586c393910c614ba3c3645a2
Marat Kopytjuk <kopytjuk@gmail.com>
2023-07-17 18:30:56 +0200

    new proposal

--- pytransform3d/transform_manager/_transform_graph_base.pyi
+++ pytransform3d/transform_manager/_transform_graph_base.pyi
@@ -16,22 +16,14 @@ class TransformGraphBase(abc.ABC):
     dist: np.ndarray
     predecessors: np.ndarray
     _cached_shortest_paths: Dict[Tuple[int, int], List[Hashable]]
+    _transforms = Dict[Tuple[Hashable, Hashable], Any]
 
     def __init__(self, strict_check: bool = ...,
                  check: bool = ...) -> "TransformGraphBase": ...
 
     @property
     @abc.abstractmethod
-    def transforms(self) -> Dict[Tuple[Hashable, Hashable], np.ndarray]: ...
-
-    @abc.abstractmethod
-    def _check_transform(self, A2B: Any) -> Any: ...
-
-    @abc.abstractmethod
-    def _invert_transform(self, A2B: Any) -> Any: ...
-
-    @abc.abstractmethod
-    def _path_transform(self, path: List[Hashable]) -> Any: ...
+    def transforms(self) -> Dict[Tuple[Hashable, Hashable], Any]: ...
 
     @abc.abstractmethod
     def _transform_available(self, key: Tuple[Hashable, Hashable]) -> bool: ...
@@ -55,9 +47,6 @@ class TransformGraphBase(abc.ABC):
     def remove_transform(
             self, from_frame: Hashable,
             to_frame: Hashable) -> "TransformGraphBase": ...
-
-    def get_transform(
-            self, from_frame: Hashable, to_frame: Hashable) -> Any: ...
 
     def _shortest_path(self, i: int, j: int) -> List[Hashable]: ...
 
