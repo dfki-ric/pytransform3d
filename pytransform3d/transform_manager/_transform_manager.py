707171d802d588c07c0151b31c980a31d9b38570
Marat Kopytjuk <kopytjuk@gmail.com>
2023-07-18 11:43:15 +0200

    remove get_transform from transform_manager

--- pytransform3d/transform_manager/_transform_manager.py
+++ pytransform3d/transform_manager/_transform_manager.py
@@ -60,51 +60,6 @@ class TransformManager(TransformGraphBase):
     def transforms(self):
         """Rigid transformations between nodes."""
         return self._transforms
-    
-    def get_transform(self, from_frame, to_frame):
-        """Request a transformation.
-
-        Parameters
-        ----------
-        from_frame : Hashable
-            Name of the frame for which the transformation is requested in the
-            to_frame coordinate system
-
-        to_frame : Hashable
-            Name of the frame in which the transformation is defined
-
-        Returns
-        -------
-        A2B : Any
-            Transformation from 'from_frame' to 'to_frame'
-
-        Raises
-        ------
-        KeyError
-            If one of the frames is unknown or there is no connection between
-            them
-        """
-        if self.check:
-            if from_frame not in self.nodes:
-                raise KeyError("Unknown frame '%s'" % from_frame)
-            if to_frame not in self.nodes:
-                raise KeyError("Unknown frame '%s'" % to_frame)
-
-        if (from_frame, to_frame) in self.transforms:
-            return self._transforms[(from_frame, to_frame)]
-
-        if (to_frame, from_frame) in self.transforms:
-            return self._invert_transform(
-                self._transforms[(to_frame, from_frame)])
-
-        i = self.nodes.index(from_frame)
-        j = self.nodes.index(to_frame)
-        if not np.isfinite(self.dist[i, j]):
-            raise KeyError("Cannot compute path from frame '%s' to "
-                           "frame '%s'." % (from_frame, to_frame))
-
-        path = self._shortest_path(i, j)
-        return self._path_transform(path)
 
     def _transform_available(self, key):
         return key in self._transforms
