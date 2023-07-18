a105be272ef3e20d0e12983bce0f1f9fb4997822
Marat Kopytjuk <kopytjuk@gmail.com>
2023-07-18 11:19:47 +0200

    add unittests

--- pytransform3d/transform_manager/__init__.py
+++ pytransform3d/transform_manager/__init__.py
@@ -4,6 +4,7 @@ See :doc:`transform_manager` for more information.
 """
 from ._transform_graph_base import TransformGraphBase
 from ._transform_manager import TransformManager
+from ._temporal_transform_manager import TemporalTransformManager, StaticTransform
 
 
-__all__ = ["TransformGraphBase", "TransformManager"]
+__all__ = ["TransformGraphBase", "TransformManager", "TemporalTransformManager", "StaticTransform"]
