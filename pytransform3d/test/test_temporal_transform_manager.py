84591cc38c32aefae2540110ca16865241bcaf02
Marat Kopytjuk <kopytjuk@gmail.com>
2023-07-18 16:51:47 +0200

    test internals

--- pytransform3d/test/test_temporal_transform_manager.py
+++ pytransform3d/test/test_temporal_transform_manager.py
@@ -6,7 +6,7 @@ from pytransform3d.transform_manager import (
 from pytransform3d.transformations import random_transform, invert_transform
 
 
-def test_request_added_transform():
+def test_temporal_transform():
     """Request an added transform from the transform manager."""
     rng = np.random.default_rng(0)
     A2B = random_transform(rng)
@@ -25,3 +25,22 @@ def test_request_added_transform():
     C2B = tm.get_transform("C", "B")
     B2C_2 = invert_transform(C2B)
     assert_array_almost_equal(B2C, B2C_2)
+
+    B2C_3 = tm.get_transform_at_time("B", "C", 1234.0)
+    assert_array_almost_equal(B2C_2, B2C_3)
+
+
+def test_internals():
+    rng = np.random.default_rng(0)
+    A2B = random_transform(rng)
+
+    rng = np.random.default_rng(42)
+    A2C = random_transform(rng)
+
+    tm = TemporalTransformManager()
+
+    tm.add_transform("A", "B", StaticTransform(A2B))
+    tm.add_transform("A", "C", StaticTransform(A2C))
+
+    tm.remove_transform("A", "C")
+    assert ("A", "C") not in tm.transforms
