f08a9c48e4fe260d9b6eddeea57f9f156786ff49
Marat Kopytjuk <kopytjuk@gmail.com>
2023-07-18 11:04:00 +0200

    refactor temporal transform manager

--- pytransform3d/transform_manager/_temporal_transform_manager.py
+++ pytransform3d/transform_manager/_temporal_transform_manager.py
@@ -20,68 +20,31 @@ class TemporalTransformManager(TransformGraphBase):
 
     def __init__(self, strict_check=True, check=True):
         self._transforms = {}
+        self._current_time = 0.0
         super(TemporalTransformManager, self).__init__(strict_check, check)
     
-    def get_transform(self, from_frame, to_frame, time):
-        """Request a transformation.
+    @property
+    def current_time(self):
+        return self._current_time
 
-        Parameters
-        ----------
-        from_frame : Hashable
-            Name of the frame for which the transformation is requested in the
-            to_frame coordinate system
-
-        to_frame : Hashable
-            Name of the frame in which the transformation is defined
-
-        Returns
-        -------
-        A2B : Any
-            Transformation from 'from_frame' to 'to_frame'
-
-        Raises
-        ------
-        KeyError
-            If one of the frames is unknown or there is no connection between
-            them
-        """
-        if self.check:
-            if from_frame not in self.nodes:
-                raise KeyError("Unknown frame '%s'" % from_frame)
-            if to_frame not in self.nodes:
-                raise KeyError("Unknown frame '%s'" % to_frame)
-
-        if (from_frame, to_frame) in self.transforms:
-            return self._transforms[(from_frame, to_frame).as_matrix(time)]
-
-        if (to_frame, from_frame) in self.transforms:
-            return self._invert_transform(
-                self._transforms[(to_frame, from_frame)].as_matrix(time))
-
-        i = self.nodes.index(from_frame)
-        j = self.nodes.index(to_frame)
-        if not np.isfinite(self.dist[i, j]):
-            raise KeyError("Cannot compute path from frame '%s' to "
-                           "frame '%s'." % (from_frame, to_frame))
-
-        path = self._shortest_path(i, j)
-        return self._path_transform(path, time)
+    def set_time(self, time):
+        self._current_time = time
     
     @property
     def transforms(self):
         """Rigid transformations between nodes."""
-        return self._transforms
+        return {tf_direction: transform.as_matrix(self.current_time) for
+                tf_direction, transform in self._transforms.items()}
 
-    def _check_transform(self, A2B):
-        return check_transform(A2B, strict_check=self.strict_check)
+    def _transform_available(self, key):
+        return key in self._transforms
 
-    def _invert_transform(self, A2B):
-        return invert_transform(
-            A2B, strict_check=self.strict_check, check=self.check)
+    def _set_transform(self, key, A2B):
+        self._transforms[key] = A2B
 
-    def _path_transform(self, path, time):
-        A2B_matrix = np.eye(4)
-        for from_f, to_f in zip(path[:-1], path[1:]):
-            A2B_matrix = concat(A2B_matrix, self.get_transform(from_f, to_f, time),
-                            strict_check=self.strict_check, check=self.check)
-        return A2B
+    def _get_transform(self, key):
+        return self.transforms[key]
+
+    def _del_transform(self, key):
+        del self._transforms[key]
+
