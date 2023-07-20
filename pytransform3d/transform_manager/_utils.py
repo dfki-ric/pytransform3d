457fd4c222c01c7b0c2ac688054e3d59e427359e
Marat Kopytjuk <kopytjuk@gmail.com>
2023-07-20 19:20:10 +0200

    improve function names

--- pytransform3d/transform_manager/_utils.py
+++ pytransform3d/transform_manager/_utils.py
@@ -3,15 +3,15 @@ import numpy as np
 from .. import transformations as pt
 
 
-def find_two_closest_indices(arr, target_value):
+def find_neighboring_indices_around_value(arr, value):
     # Calculate the absolute differences between the array elements and the target_value
-    abs_diff = np.abs(arr - target_value)
+    abs_diff = np.abs(arr - value)
 
     # Find the index of the minimum absolute difference (closest value to the target)
     closest_index = np.argmin(abs_diff)
 
     # Check if there is a match with the target_value
-    if arr[closest_index] == target_value:
+    if arr[closest_index] == value:
         return [closest_index, closest_index]
 
     # Set the selected index to a large value to exclude it from the search for the second closest
