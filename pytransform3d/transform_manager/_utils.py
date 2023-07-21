20e3ac1fd2ba1ed217f859e6b57aa718e8d72490
Marat Kopytjuk <kopytjuk@gmail.com>
2023-07-21 20:50:41 +0200

    Better function naming

--- pytransform3d/transform_manager/_utils.py
+++ pytransform3d/transform_manager/_utils.py
@@ -3,15 +3,15 @@ import numpy as np
 from .. import transformations as pt
 
 
-def find_neighboring_indices_around_value(arr, value):
+def find_neighboring_timesteps(time_arr, query_time):
     # Calculate the absolute differences between the array elements and the target_value
-    abs_diff = np.abs(arr - value)
+    abs_diff = np.abs(time_arr - query_time)
 
     # Find the index of the minimum absolute difference (closest value to the target)
     closest_index = np.argmin(abs_diff)
 
     # Check if there is a match with the target_value
-    if arr[closest_index] == value:
+    if time_arr[closest_index] == query_time:
         return [closest_index, closest_index]
 
     # Set the selected index to a large value to exclude it from the search for the second closest
