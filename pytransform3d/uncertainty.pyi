d09234d27bfc1fcce4f757e128368c616bf1839c
Alexander Fabisch <afabisch@googlemail.com>
2024-11-30 22:27:01 +0100

    Extract Frechet mean

--- pytransform3d/uncertainty.pyi
+++ pytransform3d/uncertainty.pyi
@@ -1,6 +1,6 @@
 import numpy as np
 import numpy.typing as npt
-from typing import Tuple, Union
+from typing import Tuple, Union, Callable
 from mpl_toolkits.mplot3d import Axes3D
 
 
@@ -10,6 +10,14 @@ def estimate_gaussian_rotation_matrix_from_samples(
 
 def estimate_gaussian_transform_from_samples(
         samples: npt.ArrayLike) -> Tuple[np.ndarray, np.ndarray]: ...
+
+
+def frechet_mean(
+        samples: npt.ArrayLike, mean0: npt.ArrayLike,
+        exp:Callable, log: Callable,
+        inv: Callable, concat_one_to_one: Callable,
+        concat_many_to_one: Callable,
+        n_iter: int = ...) -> Tuple[np.ndarray, np.ndarray]: ...
 
 
 def invert_uncertain_transform(
