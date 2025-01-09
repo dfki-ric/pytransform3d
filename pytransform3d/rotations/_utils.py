9bb9268f4b1e7fc8848992b8903b14f89a6cafaa
Alexander Fabisch <afabisch@googlemail.com>
2025-01-09 23:28:49 +0100

    ENH: Orthonormalization with polar decomposition

--- pytransform3d/rotations/_utils.py
+++ pytransform3d/rotations/_utils.py
@@ -52,7 +52,7 @@ def matrix_requires_renormalization(R, tolerance=1e-6):
     return not np.allclose(RRT, np.eye(3), atol=tolerance)
 
 
-def norm_matrix(R):
+def norm_matrix(R, polar_decomposition=False):
     r"""Orthonormalize rotation matrix.
 
     A rotation matrix is defined as
@@ -83,6 +83,13 @@ def norm_matrix(R):
     R : array-like, shape (3, 3)
         Rotation matrix with small numerical errors.
 
+    polar_decomposition : bool, optional (default: False)
+        Use polar decomposition [1] [2] to normalize rotation matrix. This is a
+        computationally more costly method, but it spreads the error more
+        evenly between the basis vectors. The cheaper default
+        orthonormalization method is Gram-Schmidt orthonormalization
+        optimized for 3 dimensions.
+
     Returns
     -------
     R : array, shape (3, 3)
@@ -93,7 +100,48 @@ def norm_matrix(R):
     check_matrix : Checks orthonormality of a rotation matrix.
     matrix_requires_renormalization
         Checks if a rotation matrix needs renormalization.
+
+    References
+    ----------
+    .. [1] Selstad, J. (2019). Orthonormalization.
+       https://zalo.github.io/blog/polar-decomposition/
+
+    .. [2] Müller, M., Bender, J., Chentanez, N., Macklin, M. (2016).
+       A Robust Method to Extract the Rotational Part of Deformations.
+       In MIG '16: Proceedings of the 9th International Conference on Motion in
+       Games, pp. 55-60, doi: 10.1145/2994258.2994269.
     """
+    if polar_decomposition:
+        return _polar_decomposition(R)
+    else:
+        return _gram_schmidt_orthonormalization_3d(R)
+
+
+def _polar_decomposition(R, n_iter=20, eps=np.finfo(float).eps):
+    # to avoid circular import
+    from ._conversions import (matrix_from_quaternion,
+                               quaternion_from_compact_axis_angle)
+    from ._quaternions import concatenate_quaternions
+
+    current_q = np.array([1.0, 0.0, 0.0, 0.0])
+    for _ in range(n_iter):
+        current_R = matrix_from_quaternion(current_q)
+        omega = ((np.cross(current_R[:, 0], R[:, 0])
+                  + np.cross(current_R[:, 1], R[:, 1])
+                  + np.cross(current_R[:, 2], R[:, 2]))
+                 /
+                 (abs(np.dot(current_R[:, 0], R[:, 0])
+                      + np.dot(current_R[:, 1], R[:, 1])
+                      + np.dot(current_R[:, 2], R[:, 2]))
+                  + eps))
+        if np.linalg.norm(omega) < eps:
+            break
+        current_q = concatenate_quaternions(
+            quaternion_from_compact_axis_angle(omega), current_q)
+    return matrix_from_quaternion(current_q)
+
+
+def _gram_schmidt_orthonormalization_3d(R):
     R = np.asarray(R)
     c2 = R[:, 1]
     c3 = norm_vector(R[:, 2])
