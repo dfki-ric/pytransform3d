8ad7e2af52e0296f15734c05908f7faa93bcfcb7
Alexander Fabisch <afabisch@googlemail.com>
2025-01-27 11:56:54 +0100

    DOC: Document example

--- examples/plots/plot_random_trajectories.py
+++ examples/plots/plot_random_trajectories.py
@@ -3,24 +3,31 @@
 Random Trajectories
 ===================
 
-TODO
+These plots show several randomly generated trajectories. Each row shows a
+different trajectory. On the left side you can see the position and orientation
+represented by small coordinate frames. On the right side you can see the
+positions over time.
 """
 import numpy as np
 import matplotlib.pyplot as plt
 import pytransform3d.transformations as pt
 import pytransform3d.trajectories as ptr
 
+
+# %%
+# We sample three random trajectories.
 n_trajectories = 3
-start = np.eye(4)
-goal = pt.transform_from(R=np.eye(3), p=0.3 * np.ones(3))
 trajectories = ptr.random_trajectories(
     rng=np.random.default_rng(5),
     n_trajectories=n_trajectories,
     n_steps=1001,
-    start=start,
-    goal=goal,
-    std_dev=np.array([200, 200, 200, 50, 50, 50])
+    start=np.eye(4),
+    goal=pt.transform_from(R=np.eye(3), p=0.3 * np.ones(3)),
+    std_dev=[200, 200, 200, 50, 50, 50]
 )
+
+# %%
+# We plot the trajectory in 3D on the left and in 2D on the right.
 plt.figure(figsize=(8, 8))
 for i in range(n_trajectories):
     ax = plt.subplot(n_trajectories, 2, 1 + 2 * i, projection="3d")
@@ -32,6 +39,7 @@ for i in range(n_trajectories):
     ax.set_zticks(())
     ptr.plot_trajectory(
         ax=ax, P=ptr.pqs_from_transforms(trajectories[i]), s=0.1)
+
     ax = plt.subplot(n_trajectories, 2, 2 + 2 * i)
     for d in range(3):
         plt.plot(trajectories[i, :, d, 3].T, label="XYZ"[d])
@@ -42,5 +50,6 @@ for i in range(n_trajectories):
         ax.legend(loc="best")
     ax.set_ylabel("Position")
     ax.set_xlim((0, trajectories.shape[1] - 1))
+
 plt.tight_layout()
 plt.show()
