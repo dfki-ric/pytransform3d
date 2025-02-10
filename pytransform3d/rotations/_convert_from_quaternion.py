4ee71db8e8cd07b0c1af7adbbc8b306f0ef08742
Alexander Fabisch <afabisch@googlemail.com>
2025-02-10 12:16:27 +0100

    MAINT: move functions to _convert_from_quaternion

---
+++ pytransform3d/rotations/_convert_from_quaternion.py
@@ -0,0 +1,106 @@
+import numpy as np
+from ._utils import check_quaternion, norm_axis_angle
+from ._convert_from_axis_angle import compact_axis_angle
+
+
+def matrix_from_quaternion(q):
+    """Compute rotation matrix from quaternion.
+
+    This typically results in an active rotation matrix.
+
+    Parameters
+    ----------
+    q : array-like, shape (4,)
+        Unit quaternion to represent rotation: (w, x, y, z)
+
+    Returns
+    -------
+    R : array-like, shape (3, 3)
+        Rotation matrix
+    """
+    q = check_quaternion(q, unit=True)
+    w, x, y, z = q
+    x2 = 2.0 * x * x
+    y2 = 2.0 * y * y
+    z2 = 2.0 * z * z
+    xy = 2.0 * x * y
+    xz = 2.0 * x * z
+    yz = 2.0 * y * z
+    xw = 2.0 * x * w
+    yw = 2.0 * y * w
+    zw = 2.0 * z * w
+
+    R = np.array([[1.0 - y2 - z2, xy - zw, xz + yw],
+                  [xy + zw, 1.0 - x2 - z2, yz - xw],
+                  [xz - yw, yz + xw, 1.0 - x2 - y2]])
+    return R
+
+
+def axis_angle_from_quaternion(q):
+    """Compute axis-angle from quaternion.
+
+    This operation is called logarithmic map.
+
+    We usually assume active rotations.
+
+    Parameters
+    ----------
+    q : array-like, shape (4,)
+        Unit quaternion to represent rotation: (w, x, y, z)
+
+    Returns
+    -------
+    a : array, shape (4,)
+        Axis of rotation and rotation angle: (x, y, z, angle). The angle is
+        constrained to [0, pi) so that the mapping is unique.
+    """
+    q = check_quaternion(q)
+    p = q[1:]
+    p_norm = np.linalg.norm(p)
+
+    if p_norm < np.finfo(float).eps:
+        return np.array([1.0, 0.0, 0.0, 0.0])
+
+    axis = p / p_norm
+    w_clamped = max(min(q[0], 1.0), -1.0)
+    angle = (2.0 * np.arccos(w_clamped),)
+    return norm_axis_angle(np.hstack((axis, angle)))
+
+
+def compact_axis_angle_from_quaternion(q):
+    """Compute compact axis-angle from quaternion (logarithmic map).
+
+    We usually assume active rotations.
+
+    Parameters
+    ----------
+    q : array-like, shape (4,)
+        Unit quaternion to represent rotation: (w, x, y, z)
+
+    Returns
+    -------
+    a : array, shape (3,)
+        Axis of rotation and rotation angle: angle * (x, y, z). The angle is
+        constrained to [0, pi].
+    """
+    a = axis_angle_from_quaternion(q)
+    return compact_axis_angle(a)
+
+
+def mrp_from_quaternion(q):
+    """Compute modified Rodrigues parameters from quaternion.
+
+    Parameters
+    ----------
+    q : array-like, shape (4,)
+        Unit quaternion to represent rotation: (w, x, y, z)
+
+    Returns
+    -------
+    mrp : array, shape (3,)
+        Modified Rodrigues parameters.
+    """
+    q = check_quaternion(q)
+    if q[0] < 0.0:
+        q = -q
+    return q[1:] / (1.0 + q[0])
