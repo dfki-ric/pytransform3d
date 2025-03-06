092bd6b70bfd04415283e784051d955747edc942
Alexander Fabisch <afabisch@googlemail.com>
2025-03-06 08:00:00 +0100

    MAINT: split plot_functions submodule

--- pytransform3d/plot_utils/_plot_geometries.pyi
+++ pytransform3d/plot_utils/_plot_geometries.pyi
@@ -87,22 +87,3 @@ def plot_cone(
     alpha: float = ...,
     color: str = ...,
 ) -> Axes3D: ...
-def plot_vector(
-    ax: Union[None, Axes3D] = ...,
-    start: npt.ArrayLike = ...,
-    direction: npt.ArrayLike = ...,
-    s: float = ...,
-    arrowstyle: str = ...,
-    ax_s: float = ...,
-    **kwargs,
-) -> Axes3D: ...
-def plot_length_variable(
-    ax: Union[None, Axes3D] = ...,
-    start: npt.ArrayLike = ...,
-    end: npt.ArrayLike = ...,
-    name: str = ...,
-    above: bool = ...,
-    ax_s: float = ...,
-    color: str = ...,
-    **kwargs,
-) -> Axes3D: ...
